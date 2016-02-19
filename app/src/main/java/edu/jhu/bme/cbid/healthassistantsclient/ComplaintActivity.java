92a8e1bad38f93675b644ea150011b23fb2b271b
Amal Afroz Alam <amalafrozalam@gmail.com>
2016-02-19 03:42:20 -0500

    Add screens for new patient workflow and add large landscape layouts

---
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/ComplaintActivity.java
@@ -0,0 +1,13 @@
+package edu.jhu.bme.cbid.healthassistantsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComplaintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint);
+    }
+}
---
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/PhysicalExamActivity.java
@@ -0,0 +1,13 @@
+package edu.jhu.bme.cbid.healthassistantsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhysicalExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physical_exam);
+    }
+}
