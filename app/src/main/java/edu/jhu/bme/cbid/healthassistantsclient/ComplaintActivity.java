123fb6c77b0f46d307bcf496526c2e8ae4ffb9dd
Amal Afroz Alam <amalafrozalam@gmail.com>
2016-02-19 05:01:52 -0500

    Add screens for new patient, add complaint fragments

--- app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/ComplaintActivity.java
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/ComplaintActivity.java
@@ -10,4 +10,10 @@ public class ComplaintActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
     }
+
+
+    //This activity is going to build the fragments.
+    //We're going to have the complaintcategory fragment first, on the left half of the screen.
+    //The right half of the screen will be complaintfragment
+
 }
