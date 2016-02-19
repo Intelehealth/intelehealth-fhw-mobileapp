c9f04597361fbf5e2e8d31b1fa37b4c25818306e
Amal Afroz Alam <amalafrozalam@gmail.com>
2016-02-19 02:03:42 -0500

    Add home screen buttons and identification activity.

---
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/IdentificationActivity.java
@@ -0,0 +1,13 @@
+package edu.jhu.bme.cbid.healthassistantsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IdentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification);
+    }
+}
