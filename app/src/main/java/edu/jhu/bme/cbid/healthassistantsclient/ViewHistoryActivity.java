b199f03de11a9d2b35c343ec82106737968920fa
amalafrozalam <amalafrozalam@gmail.com>
2016-03-10 15:22:34 -0500

    Add screens

---
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/ViewHistoryActivity.java
@@ -0,0 +1,13 @@
+package edu.jhu.bme.cbid.healthassistantsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+    }
+}
