44ea1763b1c3c74712cc340db63409b5b7314ac1
amalafrozalam <amalafrozalam@gmail.com>
2016-03-17 00:32:30 -0400

    Complete home screen buttons, add options menu

--- app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/MainActivity.java
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/MainActivity.java
@@ -6,7 +6,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -23,6 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Button newPtButton = (Button) findViewById(R.id.newPatientButton);
+        newPtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewPatient();
+            }
+        });
+
+
+        Button findPatientButton = (Button) findViewById(R.id.findPatientsButton);
+        findPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPatient();
+            }
+        });
+
+        Button diagnosticServicesButton = (Button) findViewById(R.id.diagnosticServicesButton);
+        findPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDiagnosticService();
+            }
+        });
+
+        Button activePatientsButton = (Button) findViewById(R.id.activePatientsButton);
+        activePatientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchActivePatients();
+            }
+        });
+
+
         // Old DatabaseIO system
         // DatabaseIO localDb = new DatabaseIO(openOrCreateDatabase("localDb", MODE_PRIVATE, null));
 
@@ -30,45 +68,64 @@ public class MainActivity extends AppCompatActivity {
         // TODO: Use SQL statements from schema
     }
 
-    public void newPatientWorkflow(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.syncOption:
+                refreshDatabases();
+                return true;
+            case R.id.settingsOption:
+                settings();
+                return true;
+            case R.id.endOfDayOption:
+                endOfDay();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startNewPatient() {
         Intent intent = new Intent(this, IdentificationActivity.class);
         startActivity(intent);
-        /*
-        Ideally, we would start this class with the fragment to do a new patient
-        See below for the reasoning behind this
-         */
     }
 
-    public void findPatientWorkflow(View view) {
+    public void findPatient() {
         Intent intent = new Intent(this, SearchPatient.class);
         startActivity(intent);
-
-        /*
-        So this should definitely start a fragment
-        Basically, this will allow the HA to search for the patient
-        If the patient is not found in the local database, give the HA the option to create a new PT
-        Then swap out the "findPatient" fragment with createPatient
-        */
     }
 
-
-    public void diagnosticTests(View view) {
+    public void selectDiagnosticService() {
         Intent intent = new Intent(this, DiagnosticTestsSelection.class);
         startActivity(intent);
-        //Listview with the diagnostic test options loaded up from a arraylist
-        //not modular, because this list will only change with a app update
-
     }
 
-
-    public void activePatients(View view) {
+    public void searchActivePatients() {
         Intent intent = new Intent(this, ActivePatientActivity.class);
         startActivity(intent);
-        // This can be a fragment, can be fragment-less as well
-
-
     }
 
+    public void refreshDatabases() {
+        // TODO: write function to sync the patients within a specific location
+        // Bandwidth heavy task
+    }
 
+    public void settings() {
+        // TODO: WTF are settings?
+    }
+
+    public void endOfDay() {
+        // TODO: sync all the data recorded to EHR, and sync back locally
+        // Information to sync includes credentials and patient info
+        // Bandwidth heavy task
+    }
 
 }
