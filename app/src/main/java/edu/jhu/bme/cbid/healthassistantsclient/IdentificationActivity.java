785bbd48f883a0d9cd43c85ca58178f748c3477f
amalafrozalam <amalafrozalam@gmail.com>
2016-03-27 06:26:51 -0400

    Validation of text input; Issue #1

--- app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/IdentificationActivity.java
+++ app/src/main/java/edu/jhu/bme/cbid/healthassistantsclient/IdentificationActivity.java
@@ -1,12 +1,22 @@
 package edu.jhu.bme.cbid.healthassistantsclient;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.gson.Gson;
+
+import edu.jhu.bme.cbid.healthassistantsclient.objects.Patient;
 
 /**
  * Created by Amal Afroz Alam on 3/25/16.
@@ -14,7 +24,27 @@ import android.widget.EditText;
 
 public class IdentificationActivity extends AppCompatActivity {
 
-    EditText mFirstName, mMiddleName, mLastName, mPhoneNum, mDOB, mAge, mAddress1, mAddress2, mCity, mState, mPostal, mCountry, mRelationship, mOccupation;
+    EditText mFirstName;
+    EditText mMiddleName;
+    EditText mLastName;
+    EditText mDOB;
+    EditText mPhoneNum;
+    EditText mAge;
+    EditText mAddress1;
+    EditText mAddress2;
+    EditText mCity;
+    EditText mState;
+    EditText mPostal;
+    EditText mCountry;
+    RadioButton mGenderM;
+    RadioButton mGenderF;
+    String mGender;
+    EditText mRelationship;
+    EditText mOccupation;
+
+    private InsertPatientTable mTask = null;
+
+    int patientID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +57,8 @@ public class IdentificationActivity extends AppCompatActivity {
         mFirstName = (EditText) findViewById(R.id.identification_first_name);
         mMiddleName = (EditText) findViewById(R.id.identification_middle_name);
         mLastName = (EditText) findViewById(R.id.identification_last_name);
+        mDOB = (EditText) findViewById(R.id.identification_birth_date);
         mPhoneNum = (EditText) findViewById(R.id.identification_phone_number);
-        mDOB = (EditText) findViewById(R.id.identification_birthdate);
         mAge = (EditText) findViewById(R.id.identification_age);
         mAddress1 = (EditText) findViewById(R.id.identification_address1);
         mAddress2 = (EditText) findViewById(R.id.identification_address2);
@@ -36,17 +66,135 @@ public class IdentificationActivity extends AppCompatActivity {
         mState = (EditText) findViewById(R.id.identification_state);
         mPostal = (EditText) findViewById(R.id.identification_postal_code);
         mCountry = (EditText) findViewById(R.id.identification_country);
+        mGenderM = (RadioButton) findViewById(R.id.identification_gender_male);
+        mGenderF = (RadioButton) findViewById(R.id.identification_gender_female);
         mRelationship = (EditText) findViewById(R.id.identification_relationship);
         mOccupation = (EditText) findViewById(R.id.identification_occupation);
+
+        mGenderF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRadioButtonClicked(v);
+            }
+        });
+
+        mGenderM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRadioButtonClicked(v);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+                createNewPatient();
+                Snackbar.make(view, R.string.snack_patient_created, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
 
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.identification_gender_male:
+                if (checked)
+                    mGender = "male";
+                break;
+            case R.id.identification_gender_female:
+                if (checked)
+                    mGender = "female";
+                break;
+        }
+    }
+
+    public void createNewPatient() {
+
+        boolean cancel = false;
+        View focusView = null;
+
+        ViewGroup identifiersLayout = (ViewGroup) findViewById(R.id.layout_identification);
+        for (int i = 0; i < identifiersLayout.getChildCount(); i++) {
+            View view = identifiersLayout.getChildAt(i);
+            if (view instanceof EditText) {
+                if (TextUtils.isEmpty(((EditText) view).getText().toString())) {
+                    if (view.getTag().toString() == "optional") {
+
+                    } else {
+
+                        ((EditText) view).setError(getString(R.string.error_field_required));
+                        focusView = view;
+                        cancel = true;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            Patient currentPatient = new Patient();
+            try {
+                currentPatient.setFirstName(mFirstName.getText().toString());
+                if (TextUtils.isEmpty(mMiddleName.getText().toString())) {
+                    currentPatient.setMiddleName("null");
+                } else {
+                    currentPatient.setMiddleName(mMiddleName.getText().toString());
+                }
+                currentPatient.setLastName(mLastName.getText().toString());
+                currentPatient.setDateOfBirth(mDOB.getText().toString());
+                currentPatient.setPhoneNumber(mPhoneNum.getText().toString());
+                currentPatient.setAddress1(mAddress1.getText().toString());
+                if (TextUtils.isEmpty(mAddress2.getText().toString())) {
+                    currentPatient.setAddress2("null");
+                } else {
+                    currentPatient.setAddress2(mAddress2.getText().toString());
+                }
+                currentPatient.setCityVillage(mCity.getText().toString());
+                currentPatient.setStateProvince(mState.getText().toString());
+                currentPatient.setCountry(mCountry.getText().toString());
+                currentPatient.setGender(mGender);
+                currentPatient.setPatientIdentifier1(mRelationship.getText().toString());
+                currentPatient.setPatientIdentifier2(mOccupation.getText().toString());
+            } catch (NullPointerException e) {
+                Snackbar.make(findViewById(R.id.cl_table), R.string.error_data_fields, Snackbar.LENGTH_SHORT);
+            }
+
+            mTask = new InsertPatientTable(currentPatient);
+            mTask.execute((Void) null);
+
+        }
+    }
+
+    public class InsertPatientTable extends AsyncTask<Void, Void, Boolean>
+            implements DialogInterface.OnCancelListener {
+
+        int patientID;
+        Patient patient;
+
+        InsertPatientTable(Patient currentPatient) {
+            patient = currentPatient;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Gson gson = new Gson();
+            Log.i("Patient", gson.toJson(patient));
+            //TODO:insert patient into DB table
+            return null;
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            cancel(true);
+            dialog.dismiss();
+        }
+    }
+
 }
+
