e6abaea49749390cf95bbba83d5e58f7677e8441
Amal Afroz Alam <contact@amal.io>
2016-09-21 21:15:19 -0400

    Change package name one more time

---
+++ app/src/main/java/io/intelehealth/telemedicine/HomeActivity.java
@@ -0,0 +1,158 @@
+package io.intelehealth.telemedicine;
+
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.acra.ACRA;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview_home);
+        recyclerView.setHasFixedSize(true);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(HomeActivity.this, 1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        recyclerView.setAdapter(new io.intelehealth.telemedicine.HomeAdapter());
+
+        // ImageButton newPtButton = (ImageButton) findViewById(R.id.newPatientButton);
+        // assert newPtButton != null;
+        // newPtButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        startNewPatient();
+        //    }
+        //});
+
+
+        // ImageButton findPatientButton = (ImageButton) findViewById(R.id.findPatientsButton);
+        // assert findPatientButton != null;
+        // findPatientButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        findPatient();
+        //    }
+        //});
+
+//        ImageButton diagnosticServicesButton = (ImageButton) findViewById(R.id.diagnosticServicesButton);
+//        assert diagnosticServicesButton != null;
+//        diagnosticServicesButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //selectDiagnosticService();
+//                Snackbar.make(v, R.string.generic_coming_soon, Snackbar.LENGTH_LONG).show();
+//            }
+//        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.syncOption:
+                refreshDatabases();
+                return true;
+            case R.id.settingsOption:
+                settings();
+                return true;
+            case R.id.endOfDayOption:
+                endOfDay();
+                return true;
+            case R.id.logoutOption:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startNewPatient() {
+        Intent intent = new Intent(this, IdentificationActivity.class);
+        startActivity(intent);
+    }
+
+    public void findPatient() {
+        Intent intent = new Intent(this, SearchPatientActivity.class);
+        startActivity(intent);
+    }
+
+    public void selectDiagnosticService() {
+
+        Intent intent = new Intent(this, DiagnosticTestsSelectionActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void refreshDatabases() {
+        // TODO: write function to sync the patients within a specific location
+        // Bandwidth heavy task
+    }
+
+    public void settings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void endOfDay() {
+        // TODO: sync all the data recorded to EHR, and sync back locally
+        // Information to sync includes credentials and patient info
+        // Bandwidth heavy task
+        try {
+            throw new NullPointerException();
+        } catch (NullPointerException caughtException) {
+            ACRA.getErrorReporter().handleException(caughtException);
+        }
+    }
+
+    public void logout() {
+        AccountManager manager = AccountManager.get(HomeActivity.this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Account[] accountList = manager.getAccountsByType("io.intelehealth.openmrs");
+        if (accountList.length > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                manager.removeAccount(accountList[0], HomeActivity.this, null, null);
+            } else {
+                manager.removeAccount(accountList[0], null, null); // Legacy implementation
+            }
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
---
+++ app/src/main/java/io/intelehealth/telemedicine/objects/Patient.java
@@ -0,0 +1,162 @@
+package io.intelehealth.telemedicine.objects;
+
+/**
+ * Patient information class for Gson data serialization
+ */
+public class Patient {
+
+    private String id;
+    private String openmrs_id;
+    private String first_name;
+    private String middle_name;
+    private String last_name;
+    private String date_of_birth; // ISO 8601
+    private String phone_number;
+    private String address1;
+    private String address2;
+    private String city_village;
+    private String state_province;
+    private String postal_code;
+    private String country; // ISO 3166-1 alpha-2
+    private String gender;
+    private String patient_photo;
+    private String sdw;
+    private String occupation;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOpenmrsId() {
+        return openmrs_id;
+    }
+
+    public void setOpenmrsId(String openmrs_id) {
+        this.openmrs_id = openmrs_id;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getMiddleName() {
+        return middle_name;
+    }
+
+    public void setMiddleName(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getDateOfBirth() {
+        return date_of_birth;
+    }
+
+    public void setDateOfBirth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCityVillage() {
+        return city_village;
+    }
+
+    public void setCityVillage(String city_village) {
+        this.city_village = city_village;
+    }
+
+    public String getStateProvince() {
+        return state_province;
+    }
+
+    public void setStateProvince(String state_province) {
+        this.state_province = state_province;
+    }
+
+    public String getPostalCode() {
+        return postal_code;
+    }
+
+    public void setPostalCode(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPatientPhoto() {
+        return patient_photo;
+    }
+
+    public void setPatientPhoto(String patient_photo) {
+        this.patient_photo = patient_photo;
+    }
+
+    public String getSdw() {
+        return sdw;
+    }
+
+    public void setSdw(String sdw) {
+        this.sdw = sdw;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
