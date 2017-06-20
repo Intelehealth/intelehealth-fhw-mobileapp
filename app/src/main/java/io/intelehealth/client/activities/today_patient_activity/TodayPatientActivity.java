9b9f7e75b66557ce64ba473e92cb888853b1f59d
Dexter Barretto <dexter@intelehealth.io>
2017-06-20 16:43:26 +0530

    Add End All Visit Menu Item to TodayPatientActivity
    End All Visit Functionality Added.
    Changes to status indicators on TodayPatientActivity.
    -Color Yellow means Visit Ended
    -Color Green means Visit Ended

--- app/src/main/java/io/intelehealth/client/activities/today_patient_activity/TodayPatientActivity.java
+++ app/src/main/java/io/intelehealth/client/activities/today_patient_activity/TodayPatientActivity.java
@@ -1,15 +1,22 @@
 
 package io.intelehealth.client.activities.today_patient_activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,14 +24,18 @@ import java.util.Date;
 import java.util.List;
 
 import io.intelehealth.client.R;
+import io.intelehealth.client.activities.home_activity.HomeActivity;
 import io.intelehealth.client.activities.today_patient_activity.today_patient_adapter.TodayPatientAdapter;
 import io.intelehealth.client.database.LocalRecordsDatabaseHelper;
 import io.intelehealth.client.objects.TodayPatientModel;
+import io.intelehealth.client.services.ClientService;
 
 /**
  * This class retrieves information about patients with visits on current date and sets data to a RecyclerView.
  */
 public class TodayPatientActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
 
     final String TAG = TodayPatientActivity.class.getSimpleName();
     Toolbar mToolbar;
@@ -45,16 +56,17 @@ public class TodayPatientActivity extends AppCompatActivity {
 
         mDbHelper = new LocalRecordsDatabaseHelper(this);
 
+        db = mDbHelper.getWritableDatabase();
         doQuery();
     }
 
     /**
      * This method retrieves visit details about patient for a particular date.
+     *
      * @return void
      */
     private void doQuery() {
         List<TodayPatientModel> todayPatientList = new ArrayList<>();
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
         Date cDate = new Date();
         String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
         String query =
@@ -101,6 +113,83 @@ public class TodayPatientActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_today_patient, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.summary_endAllVisit:
+                endAllVisit();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void endAllVisit() {
+
+        int failedUploads = 0;
+
+        String query = "SELECT visit.patient_id, visit.end_datetime, visit.openmrs_visit_uuid," +
+                "patient.first_name, patient.middle_name, patient.last_name FROM visit, patient WHERE" +
+                " visit.patient_id = patient._id AND visit.end_datetime IS NULL OR visit.end_datetime = ''";
+
+        final Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    boolean result = endVisit(
+                            cursor.getString(cursor.getColumnIndexOrThrow("patient_id")),
+                            cursor.getString(cursor.getColumnIndexOrThrow("first_name")) + " " +
+                                    cursor.getString(cursor.getColumnIndexOrThrow("last_name")),
+                            cursor.getString(cursor.getColumnIndexOrThrow("openmrs_visit_uuid"))
+                    );
+                    if(!result) failedUploads++;
+                } while (cursor.moveToNext());
+            }
+        }
+        cursor.close();
+
+        if (failedUploads == 0) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+        else{
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Unable to end "+failedUploads +
+                    " visits.Please upload visit before attempting to end the visit.");
+            alertDialogBuilder.setNeutralButton(R.string.generic_ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
+    }
+
+    private boolean endVisit(String patientID, String patientName, String visitUUID) {
+
+        if (visitUUID == null) {
+            return false;
+        } else {
+            Intent serviceIntent = new Intent(this, ClientService.class);
+            serviceIntent.putExtra("serviceCall", "endVisit");
+            serviceIntent.putExtra("patientID", patientID);
+            serviceIntent.putExtra("visitUUID", visitUUID);
+            serviceIntent.putExtra("name", patientName);
+            startService(serviceIntent);
+            return true;
+        }
+
+    }
 }
 
