d162f245fb44a8df47f342a322d37dea9a5559d5
Harshish <harshishyadav@gmail.com>
2017-07-04 16:13:48 +0530

    Integrating image and mind-map Server

--- app/src/main/java/io/intelehealth/client/utilities/HelperMethods.java
+++ app/src/main/java/io/intelehealth/client/utilities/HelperMethods.java
@@ -51,7 +51,7 @@ public class HelperMethods {
 
     public static final String MIND_MAP_SERVER_URL = "http://139.59.73.230:1337/parse/";
     public static final String MIND_MAP_APP_ID = "app";
-    public static final String IMAGE_SERVER_URL = "http://139.59.73.230:13'38/parse/";
+    public static final String IMAGE_SERVER_URL = "http://139.59.73.230:1338/parse/";
     public static final String IMAGE_APP_ID = "app2";
     public static final String JSON_FOLDER = "Engines";
     public static File base_dir;
