a0a18f423981ed0f9294cb21e98a540787c98668
Amal Afroz Alam <contact@amal.io>
2018-03-24 23:55:43 -0400

    WIP #436

---
+++ app/src/main/java/io/intelehealth/client/activities/patient_exit_survey_activity/PatientExitSurveyActivity.java
@@ -0,0 +1,18 @@
+package io.intelehealth.client.activities.patient_exit_survey_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import io.intelehealth.client.R;
+
+public class PatientExitSurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_exit_survey);
+    }
+
+
+
+
+}
