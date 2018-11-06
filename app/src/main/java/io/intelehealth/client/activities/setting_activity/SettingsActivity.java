f28e728d06c576193e24724794d3c28a58234a1b
Meera Potdar <meera@intelehealth.io>
2018-11-06 18:37:58 +0530

    #521-Language bug
    Added code to make sure that the language is added to shared preferences
    Some strings translation not provided in strings.xml

--- app/src/main/java/io/intelehealth/client/activities/setting_activity/SettingsActivity.java
+++ app/src/main/java/io/intelehealth/client/activities/setting_activity/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.media.Ringtone;
@@ -302,17 +303,35 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         Preference lang_prefer;
 
         public void setLocale(String lang) {
+            if (lang.equalsIgnoreCase(""))
+                return;
             final Locale myLocale = new Locale(lang);
+            Locale.setDefault(myLocale);
+            saveLocale(lang);
             Resources res = getResources();
             DisplayMetrics dm = res.getDisplayMetrics();
             Configuration conf = res.getConfiguration();
             conf.locale = myLocale;
             res.updateConfiguration(conf, dm);
-           //Intent refresh = new Intent(this, HomeActivity.class);
-           //finish();
-           // startActivity(refresh);
+            //Intent refresh = new Intent(this, HomeActivity.class);
+            //finish();
+            // startActivity(refresh);
         }
 
+
+        public void loadLocale() {
+            String langPref = "Language";
+            SharedPreferences prefs = this.getActivity().getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+            String language = prefs.getString(langPref, "");
+            setLocale(language);
+        }
+        public void saveLocale(String lang) {
+            String langPref = "Language";
+            SharedPreferences prefs = this.getActivity().getSharedPreferences("CommonPrefs",Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(langPref, lang);
+            editor.commit();
+        }
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -344,6 +363,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                                             ? listPreference.getEntries()[index]
                                             : null);
                           setLocale(stringValue);
+                            loadLocale();
                             //Intent refresh = new Intent(this, HomeActivity.class);
                             return true;
                         }}
