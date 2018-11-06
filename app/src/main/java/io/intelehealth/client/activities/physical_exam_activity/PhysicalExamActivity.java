14998507ba12746b66b33d6091e57a213087f488
Meera Potdar <meera@intelehealth.io>
2018-11-06 18:40:40 +0530

    #555-Handwashing prompt made visual

--- app/src/main/java/io/intelehealth/client/activities/physical_exam_activity/PhysicalExamActivity.java
+++ app/src/main/java/io/intelehealth/client/activities/physical_exam_activity/PhysicalExamActivity.java
@@ -116,6 +116,9 @@ public class PhysicalExamActivity extends AppCompatActivity {
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage(R.string.wash_hands);
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.hand_wash, null);
+        alertDialogBuilder.setView(view);
         alertDialogBuilder.setNeutralButton(R.string.generic_ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
