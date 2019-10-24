c46ae29ecc883d07fd4c6ef3880c20a95765e1ba
Prajwal_W <prajwalwaingankar@yahoo.in>
2019-10-24 16:32:37 +0530

    Notification Channel code added to support notifications for >=Orea

--- app/src/main/java/io/intelehealth/client/services/firebase_services/MyFirebaseMessagingService.java
+++ app/src/main/java/io/intelehealth/client/services/firebase_services/MyFirebaseMessagingService.java
@@ -1,11 +1,13 @@
 package io.intelehealth.client.services.firebase_services;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -61,6 +63,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        String channelId = "CHANNEL_ID";
+
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
@@ -72,6 +76,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel
+                    (channelId, "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationManager.createNotificationChannel(channel);
+        }
 
         notificationManager.notify(0, notificationBuilder.build());
     }
