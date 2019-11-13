65adf0ddb04e4bcee6e74288a5d34481ba0b6963
Sagar S <sagar@intelehealth.io>
2019-11-13 15:37:06 +0530

    Done minor change for triggering prescription notifications as change into Sync functionality

---
+++ app/src/main/java/io/intelehealth/client/utilities/NotificationID.java
@@ -0,0 +1,15 @@
+package io.intelehealth.client.utilities;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Sagar Shimpi
+ * Github - sagarS23
+ */
+public class NotificationID {
+    private final static AtomicInteger c = new AtomicInteger(5);
+
+    public static int getID() {
+        return c.incrementAndGet();
+    }
+}
