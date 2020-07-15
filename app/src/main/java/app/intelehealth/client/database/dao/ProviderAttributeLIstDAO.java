606a1bc74a176d8827b434c1273131296742bdf3
prajwalmw <prajwalwaingankar@yahoo.in>
2020-07-15 19:26:47 +0530

    get_uuid&values function added with model class.

--- app/src/main/java/app/intelehealth/client/database/dao/ProviderAttributeLIstDAO.java
+++ app/src/main/java/app/intelehealth/client/database/dao/ProviderAttributeLIstDAO.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import app.intelehealth.client.app.AppConstants;
+import app.intelehealth.client.models.Uuid_Value;
 import app.intelehealth.client.models.dto.ProviderAttributeListDTO;
 import app.intelehealth.client.models.dto.ProviderDTO;
 import app.intelehealth.client.models.dto.VisitDTO;
@@ -111,6 +112,32 @@ public class ProviderAttributeLIstDAO {
         db.close();
         return listDTOArrayList;
     }
+
+
+    public List<Uuid_Value> getSpeciality_Uuid_Value() {
+        List<Uuid_Value> listDTOArrayList = new ArrayList<>();
+        SQLiteDatabase db = AppConstants.inteleHealthDatabaseHelper.getWritableDatabase();
+        db.beginTransaction();
+        String selectionArgs[] = {"ed1715f5-93e2-404e-b3c9-2a2d9600f062", "0"};
+        Cursor idCursor = db.rawQuery("SELECT * FROM tbl_dr_speciality WHERE " +
+                "attributetypeuuid = ? AND voided = ?", selectionArgs);
+
+        ProviderAttributeListDTO dto = new ProviderAttributeListDTO();
+        if (idCursor.getCount() != 0) {
+            while (idCursor.moveToNext()) {
+                dto = new ProviderAttributeListDTO();
+                dto.setUuid(idCursor.getString(idCursor.getColumnIndexOrThrow("uuid")));
+                dto.setValue(idCursor.getString(idCursor.getColumnIndexOrThrow("value")));
+                listDTOArrayList.add(new Uuid_Value(dto.getUuid(), dto.getValue()));
+            }
+        }
+        idCursor.close();
+        db.setTransactionSuccessful();
+        db.endTransaction();
+        db.close();
+        return listDTOArrayList;
+    }
+
 
 
 
