f5215f4ae1bc934875f6fcdf2c9c3d8d1d263f37
prajwalmw <prajwalwaingankar@yahoo.in>
2020-07-16 17:14:21 +0530

    VisitAttributespecilaity DTO is generated.

---
+++ app/src/main/java/app/intelehealth/client/models/dto/VisitAttribute_Speciality.java
@@ -0,0 +1,34 @@
+package app.intelehealth.client.models.dto;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+/**
+ * Created by Prajwal Waingankar
+ * on 16-Jul-20.
+ * Github: prajwalmw
+ */
+
+public class VisitAttribute_Speciality {
+    
+    @SerializedName("attributeType")
+    @Expose
+    private String attributeType;
+    @SerializedName("value")
+    @Expose
+    private String value;
+
+    public String getAttributeType() {
+        return attributeType;
+    }
+
+    public void setAttributeType(String attributeType) {
+        this.attributeType = attributeType;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
