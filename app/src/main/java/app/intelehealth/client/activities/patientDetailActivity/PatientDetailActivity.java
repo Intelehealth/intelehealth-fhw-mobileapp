80deb1def3923e06596076c005f533427b767ad5
prajwalmw <prajwalwaingankar@yahoo.in>
2020-09-23 16:15:04 +0530

    Converted to Bitmap.

--- app/src/main/java/app/intelehealth/client/activities/patientDetailActivity/PatientDetailActivity.java
+++ app/src/main/java/app/intelehealth/client/activities/patientDetailActivity/PatientDetailActivity.java
@@ -460,6 +460,7 @@ public class PatientDetailActivity extends AppCompatActivity {
 
         Glide.with(PatientDetailActivity.this)
                 .load(patient_new.getPatient_photo())
+                .asBitmap()
                 .thumbnail(0.3f)
                 .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -619,6 +620,7 @@ public class PatientDetailActivity extends AppCompatActivity {
                         if (updated) {
                             Glide.with(PatientDetailActivity.this)
                                     .load(AppConstants.IMAGE_PATH + patientUuid + ".jpg")
+                                    .asBitmap()
                                     .thumbnail(0.3f)
                                     .centerCrop()
                                     .diskCacheStrategy(DiskCacheStrategy.NONE)
