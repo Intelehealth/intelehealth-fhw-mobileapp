1cc5b1a83df735deba6615b3c8e57faa2a9bda3e
Sagar <seasapps@gmail,.com>
2020-10-26 17:05:52 +0530

    Changed package name

---
+++ app/src/main/java/org/intelehealth/ekalarogya/networkApiCalls/ApiInterface.java
@@ -0,0 +1,92 @@
+package org.intelehealth.ekalarogya.networkApiCalls;
+
+
+import org.intelehealth.ekalarogya.models.CheckAppUpdateRes;
+import org.intelehealth.ekalarogya.models.DownloadMindMapRes;
+import org.intelehealth.ekalarogya.models.Location;
+import org.intelehealth.ekalarogya.models.ObsImageModel.ObsJsonResponse;
+import org.intelehealth.ekalarogya.models.ObsImageModel.ObsPushDTO;
+import org.intelehealth.ekalarogya.models.Results;
+import org.intelehealth.ekalarogya.models.dto.ResponseDTO;
+import org.intelehealth.ekalarogya.models.loginModel.LoginModel;
+import org.intelehealth.ekalarogya.models.loginProviderModel.LoginProviderModel;
+import org.intelehealth.ekalarogya.models.patientImageModelRequest.PatientProfile;
+import org.intelehealth.ekalarogya.models.pushRequestApiCall.PushRequestApiCall;
+import org.intelehealth.ekalarogya.models.pushResponseApiCall.PushResponseApiCall;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+import retrofit2.http.Url;
+
+public interface ApiInterface {
+
+
+    @GET("location?tag=Login%20Location")
+    Observable<Results<Location>> LOCATION_OBSERVABLE(@Query("v") String representation);
+
+
+    @DELETE
+    Call<Void> DELETE_ENCOUNTER(@Url String url,
+                                @Header("Authorization") String authHeader);
+
+    //EMR-Middleware/webapi/pull/pulldata/
+    @GET
+    Call<ResponseDTO> RESPONSE_DTO_CALL(@Url String url,
+                                        @Header("Authorization") String authHeader);
+
+    @GET
+    Observable<LoginModel> LOGIN_MODEL_OBSERVABLE(@Url String url,
+                                                  @Header("Authorization") String authHeader);
+
+    @GET
+    Observable<LoginProviderModel> LOGIN_PROVIDER_MODEL_OBSERVABLE(@Url String url,
+                                                                   @Header("Authorization") String authHeader);
+
+    @Headers({"Accept: application/json"})
+    @POST
+    Single<PushResponseApiCall> PUSH_RESPONSE_API_CALL_OBSERVABLE(@Url String url,
+                                                                  @Header("Authorization") String authHeader,
+                                                                  @Body PushRequestApiCall pushRequestApiCall);
+
+    @GET
+    Observable<ResponseBody> PERSON_PROFILE_PIC_DOWNLOAD(@Url String url,
+                                                         @Header("Authorization") String authHeader);
+
+    @POST
+    Single<ResponseBody> PERSON_PROFILE_PIC_UPLOAD(@Url String url,
+                                                   @Header("Authorization") String authHeader,
+                                                   @Body PatientProfile patientProfile);
+
+    @GET
+    Observable<ResponseBody> OBS_IMAGE_DOWNLOAD(@Url String url,
+                                                @Header("Authorization") String authHeader);
+
+    @Headers({"Accept: application/json"})
+    @POST
+    @Multipart
+    Observable<ObsJsonResponse> OBS_JSON_RESPONSE_OBSERVABLE(@Url String url,
+                                                             @Header("Authorization") String authHeader,
+                                                             @Part MultipartBody.Part image,
+                                                             @Part("json") ObsPushDTO obsJsonRequest);
+
+    @DELETE
+    Observable<Void> DELETE_OBS_IMAGE(@Url String url, @Header("Authorization") String authHeader);
+
+
+    @GET("/api/mindmap/download")
+    Observable<DownloadMindMapRes> DOWNLOAD_MIND_MAP_RES_OBSERVABLE(@Query("key") String licenseKey);
+
+    @GET("/intelehealth/app_update.json")
+    Single<CheckAppUpdateRes> checkAppUpdate();
+}
---
+++ app/src/main/java/org/intelehealth/ekalarogya/utilities/IReturnValues.java
@@ -0,0 +1,5 @@
+package org.intelehealth.ekalarogya.utilities;
+
+public interface IReturnValues {
+    void onReturnValue(String value);
+}
