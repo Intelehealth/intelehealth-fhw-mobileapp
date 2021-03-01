43e12ae757edc7bdf68e1239486aea69e58ef6fe
Prajwal W <prajwalwaingankar@gmail.com>
2021-03-01 20:15:47 +0530

    Flash Off is set as default.

--- app/src/main/java/app/intelehealth/client/activities/cameraActivity/CameraActivity.java
+++ app/src/main/java/app/intelehealth/client/activities/cameraActivity/CameraActivity.java
@@ -69,18 +69,18 @@ public class CameraActivity extends AppCompatActivity {
      */
     public static final String SHOW_DIALOG_MESSAGE = "DEFAULT_DLG";
     private static final int[] FLASH_OPTIONS = {
-            CameraView.FLASH_AUTO,
             CameraView.FLASH_OFF,
+            CameraView.FLASH_AUTO,
             CameraView.FLASH_ON,
     };
     private static final int[] FLASH_ICONS = {
-            R.drawable.ic_flash_auto,
             R.drawable.ic_flash_off,
+            R.drawable.ic_flash_auto,
             R.drawable.ic_flash_on,
     };
     private static final int[] FLASH_TITLES = {
-            R.string.flash_auto,
             R.string.flash_off,
+            R.string.flash_auto,
             R.string.flash_on,
     };
     private final String TAG = CameraActivity.class.getSimpleName();
