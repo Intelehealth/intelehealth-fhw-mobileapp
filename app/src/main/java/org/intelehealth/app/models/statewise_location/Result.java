a9f5d06d01d97b6cf15588b970b20f6619a62d08
SagarS23 <seasapps@gmail,.com>
2021-05-01 13:43:22 +0530

    Changed package name, added some strings, firebase and minor changes

---
+++ app/src/main/java/org/intelehealth/app/models/statewise_location/Result.java
@@ -0,0 +1,37 @@
+package org.intelehealth.app.models.statewise_location;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Prajwal Waingankar
+ * on 27-Jan-2021.
+ * Github: prajwalmw
+ */
+
+public class Result {
+
+    @SerializedName("uuid")
+    @Expose
+    private String uuid;
+    @SerializedName("display")
+    @Expose
+    private String display;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+}
---
+++ app/src/main/java/org/intelehealth/app/syncModule/VisitSummaryWork.java
@@ -0,0 +1,48 @@
+package org.intelehealth.app.syncModule;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
+import androidx.annotation.NonNull;
+import androidx.work.ListenableWorker;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+
+
+import org.intelehealth.app.app.IntelehealthApplication;
+import org.intelehealth.app.utilities.Logger;
+import org.intelehealth.app.utilities.SessionManager;
+
+
+public class VisitSummaryWork extends Worker {
+
+    private SessionManager sessionManager = null;
+    private String TAG = VisitSummaryWork.class.getSimpleName();
+
+    public VisitSummaryWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        sessionManager = new SessionManager(context);
+    }
+
+
+    @NonNull
+    @Override
+    public ListenableWorker.Result doWork() {
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            FirebaseCrashlytics.getInstance().recordException(e);
+            Logger.logE(TAG, "Exception in doWork method", e);
+        }
+        Logger.logD(TAG, "doWork");
+
+        Intent in = new Intent();
+        in.setAction("downloadprescription");
+        IntelehealthApplication.getAppContext().sendBroadcast(in);
+
+        return Result.success();
+    }
+}
