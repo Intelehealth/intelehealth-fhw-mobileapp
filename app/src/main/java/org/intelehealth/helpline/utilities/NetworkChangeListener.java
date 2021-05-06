e0517b8576b96627c5afb2e7bb56057e3f599c07
Nishita <goyalnishita07@gmail.com>
2021-05-06 12:30:30 +0530

    Handle Condition with No internet on SetupActivity

---
+++ app/src/main/java/org/intelehealth/helpline/utilities/NetworkChangeListener.java
@@ -0,0 +1,16 @@
+package org.intelehealth.helpline.utilities;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public abstract class NetworkChangeListener extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        String status = NetworkConnection.getConnectivityStatusString(context);
+        onNetworkChange(status);
+    }
+
+    protected abstract void onNetworkChange(String status);
+}
