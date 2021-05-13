294dca2dde461b8e195b742fb449dfb657ddf091
Nishita <goyalnishita07@gmail.com>
2021-05-13 17:19:46 +0530

    Privacy Notice support added for Marathi and Gujarati

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
@@ -127,7 +127,27 @@ public class PrivacyNotice_Activity extends AppCompatActivity implements View.On
                     privacy_textview.setText(privacy_string);
                 }
 
-            } else {
+            }
+            if (current.toString().equals("gu")) {
+                String privacy_string = obj.getString("privacyNoticeText_Gujarati");
+                if (privacy_string.isEmpty()) {
+                    privacy_string = obj.getString("privacyNoticeText");
+                    privacy_textview.setText(privacy_string);
+                } else {
+                    privacy_textview.setText(privacy_string);
+                }
+
+            }
+            if (current.toString().equals("mr")) {
+                String privacy_string = obj.getString("privacyNoticeText_Marathi");
+                if (privacy_string.isEmpty()) {
+                    privacy_string = obj.getString("privacyNoticeText");
+                    privacy_textview.setText(privacy_string);
+                } else {
+                    privacy_textview.setText(privacy_string);
+                }
+
+            }else {
                 String privacy_string = obj.getString("privacyNoticeText");
                 privacy_textview.setText(privacy_string);
             }
