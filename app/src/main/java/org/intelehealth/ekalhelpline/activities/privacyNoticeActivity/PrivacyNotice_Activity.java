18f5409a7f44c935bff2b17e1bf25a194e21ed75
Prajwal W <prajwalwaingankar@gmail.com>
2021-05-18 18:46:56 +0530

    Assamese language support added in app + Privacy notice support added

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
@@ -127,7 +127,18 @@ public class PrivacyNotice_Activity extends AppCompatActivity implements View.On
                     privacy_textview.setText(privacy_string);
                 }
 
-            } else {
+            }
+            //privacy notice support for Assamese language...
+            else if (current.toString().equals("as")) {
+                String privacy_string = obj.getString("privacyNoticeText_Assamese");
+                if (privacy_string.isEmpty()) {
+                    privacy_string = obj.getString("privacyNoticeText");
+                    privacy_textview.setText(privacy_string);
+                } else {
+                    privacy_textview.setText(privacy_string);
+                }
+            }
+            else {
                 String privacy_string = obj.getString("privacyNoticeText");
                 privacy_textview.setText(privacy_string);
             }
