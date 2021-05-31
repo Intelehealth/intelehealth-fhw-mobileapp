9bd074886bbc25536b518cccdc3d06c6709e9a18
Prajwal W <prajwalwaingankar@gmail.com>
2021-05-31 16:55:12 +0530

    BEngali language support added in app + Privacy notice support added

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
@@ -165,6 +165,16 @@ public class PrivacyNotice_Activity extends AppCompatActivity implements View.On
                     privacy_textview.setText(privacy_string);
                 }
             }
+            //privacy notice support for Bengali language...
+            else if (current.toString().equals("bn")) {
+                String privacy_string = obj.getString("privacyNoticeText_Bengali");
+                if (privacy_string.isEmpty()) {
+                    privacy_string = obj.getString("privacyNoticeText");
+                    privacy_textview.setText(privacy_string);
+                } else {
+                    privacy_textview.setText(privacy_string);
+                }
+            }
             else {
                 String privacy_string = obj.getString("privacyNoticeText");
                 privacy_textview.setText(privacy_string);
