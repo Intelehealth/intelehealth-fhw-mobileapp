e84de672d32e20a0794f6b1ac618282b71be5f8f
Nishita <goyalnishita07@gmail.com>
2021-06-28 16:51:15 +0530

    App logo changed to intelehealth logo

--- app/src/main/java/org/intelehealth/ekalhelpline/services/firebase_services/MyFirebaseMessagingService.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/services/firebase_services/MyFirebaseMessagingService.java
@@ -67,7 +67,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.mipmap.ic_launcher_intelehealth)
                 .setContentTitle("Firebase Push Notification")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
