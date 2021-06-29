014cb81f870086d0c3ea6dc57e03f76a058a4104
Nishita <goyalnishita07@gmail.com>
2021-06-29 11:45:39 +0530

    Gender null issue fixed on VisitSummaryActivity

--- app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
@@ -1157,6 +1157,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
                 Intent intent1 = new Intent(VisitSummaryActivity.this, VitalsActivity.class);
                 intent1.putExtra("patientUuid", patientUuid);
                 intent1.putExtra("visitUuid", visitUuid);
+                intent1.putExtra("gender", patientGender);
                 intent1.putExtra("encounterUuidVitals", encounterVitals);
                 intent1.putExtra("encounterUuidAdultIntial", encounterUuidAdultIntial);
                 intent1.putExtra("name", patientName);
@@ -1234,6 +1235,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
                         intent1.putExtra("visitUuid", visitUuid);
                         intent1.putExtra("encounterUuidVitals", encounterVitals);
                         intent1.putExtra("edit_FamHist", "edit_FamHist");
+                        intent1.putExtra("gender", patientGender);
                      /*   if(EncounterAdultInitial_LatestVisit != null &&
                                 !EncounterAdultInitial_LatestVisit.isEmpty()) {
                             intent1.putExtra("EncounterAdultInitial_LatestVisit", EncounterAdultInitial_LatestVisit);
@@ -1341,6 +1343,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
                         Intent intent1 = new Intent(VisitSummaryActivity.this, ComplaintNodeActivity.class);
                         intent1.putExtra("patientUuid", patientUuid);
                         intent1.putExtra("visitUuid", visitUuid);
+                        intent1.putExtra("gender", patientGender);
                         intent1.putExtra("encounterUuidVitals", encounterVitals);
                         intent1.putExtra("encounterUuidAdultIntial", encounterUuidAdultIntial);
                         intent1.putExtra("name", patientName);
@@ -1448,6 +1451,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
                         intent1.putExtra("patientUuid", patientUuid);
                         intent1.putExtra("visitUuid", visitUuid);
                         intent1.putExtra("encounterUuidVitals", encounterVitals);
+                        intent1.putExtra("gender", patientGender);
                         intent1.putExtra("encounterUuidAdultIntial", encounterUuidAdultIntial);
                         intent1.putExtra("name", patientName);
                         intent1.putExtra("float_ageYear_Month", float_ageYear_Month);
@@ -1544,6 +1548,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
                         intent1.putExtra("visitUuid", visitUuid);
                         intent1.putExtra("encounterUuidVitals", encounterVitals);
                         intent1.putExtra("edit_PatHist", "edit_PatHist");
+                        intent1.putExtra("gender", patientGender);
 //                        intent1.putExtra("encounterUuidAdultIntial", encounterUuidAdultIntial);
                       /*  if(EncounterAdultInitial_LatestVisit != null &&
                                 !EncounterAdultInitial_LatestVisit.isEmpty()) {
