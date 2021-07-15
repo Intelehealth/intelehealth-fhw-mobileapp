22bc5cc648cf6d3ea85e5dbb5ce0942d2f8d0037
Prajwal W <prajwalwaingankar@gmail.com>
2021-07-15 17:16:57 +0530

    Fixed medical advice sync issues

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/patientDetailActivity/PatientDetailActivity.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/patientDetailActivity/PatientDetailActivity.java
@@ -55,6 +55,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.intelehealth.ekalhelpline.R;
 import org.intelehealth.ekalhelpline.app.AppConstants;
@@ -1004,10 +1005,18 @@ public class PatientDetailActivity extends AppCompatActivity {
             Date startTime = startFormat.parse(datetime);
             Date endTime = endFormat.parse(end_datetime);
             long diff = endTime.getTime() - startTime.getTime();
-            if (diff == 60000 * 5)
+            if (diff == TimeUnit.MINUTES.toMillis(5))
                 return true;
         } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                Date startTime = startFormat.parse(datetime);
+                Date endTime = startFormat.parse(end_datetime);
+                long diff = endTime.getTime() - startTime.getTime();
+                if (diff == TimeUnit.MINUTES.toMillis(5))
+                    return true;
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
         }
 
         return false;
