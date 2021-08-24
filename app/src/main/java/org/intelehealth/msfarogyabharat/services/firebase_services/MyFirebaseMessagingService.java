1b5af395c5e75bc3e0b09389a7f2df430510ff5c
Ritika <ritika@intelehealth.org>
2021-08-24 18:02:00 +0530

    Added the App Logo.
    Also change the name of app  to vikalp helpline

--- app/src/main/java/org/intelehealth/msfarogyabharat/services/firebase_services/MyFirebaseMessagingService.java
+++ app/src/main/java/org/intelehealth/msfarogyabharat/services/firebase_services/MyFirebaseMessagingService.java
@@ -67,7 +67,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher_intelehealth)
+                .setSmallIcon(R.mipmap.ic_launcher_ibis)
                 .setContentTitle("Firebase Push Notification")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
