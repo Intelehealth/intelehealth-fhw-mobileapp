178138d2c222a4ab8020b97d5d3ed1b2e634e043
Manoj Pedvi <manoj.pedvi@gmail.com>
2021-09-16 17:24:37 +0530

    Fixed crash on patient detail reported by Prithi

--- app/src/main/java/org/intelehealth/msfarogyabharat/activities/patientDetailActivity/PatientDetailActivity.java
+++ app/src/main/java/org/intelehealth/msfarogyabharat/activities/patientDetailActivity/PatientDetailActivity.java
@@ -1592,7 +1592,11 @@ public class PatientDetailActivity extends AppCompatActivity {
                                 for (String comp : complaints) {
                                     if (!comp.trim().isEmpty()) {
                                         Log.d("colon", "colon: " + comp);
-                                        visitValue = visitValue + Node.bullet_arrow + comp.substring(0, comp.indexOf(colon)) + "<br/>";
+                                        if (comp.contains(colon)) {
+                                            visitValue = visitValue + Node.bullet_arrow + comp.substring(0, comp.indexOf(colon)) + "<br/>";
+                                        } else {
+                                            visitValue = visitValue + Node.bullet_arrow + comp;
+                                        }
                                         Log.d("colon", "colon_visitvalue: " + visitValue);
                                     }
                                 }
