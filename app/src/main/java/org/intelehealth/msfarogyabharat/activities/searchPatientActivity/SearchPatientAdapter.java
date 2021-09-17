6a173fde2fe659f5f388af304eed548914529854
Manoj Pedvi <manoj.pedvi@gmail.com>
2021-09-17 16:32:37 +0530

    Hide comment on search listing

--- app/src/main/java/org/intelehealth/msfarogyabharat/activities/searchPatientActivity/SearchPatientAdapter.java
+++ app/src/main/java/org/intelehealth/msfarogyabharat/activities/searchPatientActivity/SearchPatientAdapter.java
@@ -57,12 +57,12 @@ public class SearchPatientAdapter extends RecyclerView.Adapter<SearchPatientAdap
                 holder.headTextView.setText(patinet.getFirstname() + " " + patinet.getLastname());
 
             holder.bodyTextView.setText(body);
-            if (TextUtils.isEmpty(patinet.comment)) {
-                holder.commentTextView.setVisibility(View.GONE);
-            } else {
-                holder.commentTextView.setText(patinet.comment);
-                holder.commentTextView.setVisibility(View.VISIBLE);
-            }
+//            if (TextUtils.isEmpty(patinet.comment)) {
+//                holder.commentTextView.setVisibility(View.GONE);
+//            } else {
+//                holder.commentTextView.setText(patinet.comment);
+//                holder.commentTextView.setVisibility(View.VISIBLE);
+//            }
         }
         holder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,13 +92,13 @@ public class SearchPatientAdapter extends RecyclerView.Adapter<SearchPatientAdap
         LinearLayout linearLayout;
         private TextView headTextView;
         private TextView bodyTextView;
-        private TextView commentTextView;
+//        private TextView commentTextView;
 
         public Myholder(View itemView) {
             super(itemView);
             headTextView = itemView.findViewById(R.id.list_item_head);
             bodyTextView = itemView.findViewById(R.id.list_item_body);
-            commentTextView = itemView.findViewById(R.id.list_item_comment);
+//            commentTextView = itemView.findViewById(R.id.list_item_comment);
             linearLayout = itemView.findViewById(R.id.searchlinear);
         }
     }
