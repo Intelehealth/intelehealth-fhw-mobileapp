27c6c6c1a363bd8125aaf4f7c117ba896ccca883
Nishita Goyal <goyalnishita07@gmail.com>
2021-09-23 18:01:21 +0530

    EditText remains focused even when clicked outside - Fixed

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/identificationActivity/IdentificationActivity.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/identificationActivity/IdentificationActivity.java
@@ -10,6 +10,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 
 import com.google.android.material.checkbox.MaterialCheckBox;
@@ -29,6 +30,7 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -4083,4 +4085,20 @@ public class IdentificationActivity extends AppCompatActivity {
         return new String(buffer, "UTF-8");
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
 }
