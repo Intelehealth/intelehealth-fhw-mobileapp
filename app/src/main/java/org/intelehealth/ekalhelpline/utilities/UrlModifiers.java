7056879fe6800842da02ec482176f8016058e930
Nishita Goyal <goyalnishita07@gmail.com>
2021-09-29 16:02:25 +0530

    Custom value is send through calling api

--- app/src/main/java/org/intelehealth/ekalhelpline/utilities/UrlModifiers.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/utilities/UrlModifiers.java
@@ -87,7 +87,7 @@ public class UrlModifiers {
 
     public String getIvrCallUrl(String caller, String receiver) {
         String api_key = "A4f98feaafc067dd6d8d5223762e9ad44";
-        return String.format("https://api-voice.kaleyra.com/v1/?api_key=%s&method=dial.click2call&caller=%s&receiver=%s", api_key, caller, receiver);
+        return String.format("https://api-voice.kaleyra.com/v1/?api_key=%s&method=dial.click2call&caller=%s&receiver=%s&custom=app", api_key, caller, receiver);
     }
 
     public String getIvrCall_ResponseUrl(String receiver, String todayDate) {
