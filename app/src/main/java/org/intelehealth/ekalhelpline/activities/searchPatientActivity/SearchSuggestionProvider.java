4ef2876dfa016b740661204103ac58c98d8a3c9f
Prajwal W <prajwalwaingankar@gmail.com>
2021-10-07 14:05:11 +0530

    Search authority for content provider path is corrected.

--- app/src/main/java/org/intelehealth/ekalhelpline/activities/searchPatientActivity/SearchSuggestionProvider.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/activities/searchPatientActivity/SearchSuggestionProvider.java
@@ -9,7 +9,7 @@ import android.content.SearchRecentSuggestionsProvider;
  */
 
 public class SearchSuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "SearchSuggestionProvider";
+    public final static String AUTHORITY = "org.intelehealth.ekalhelpline.activities.searchPatientActivity.SearchSuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public SearchSuggestionProvider() {
