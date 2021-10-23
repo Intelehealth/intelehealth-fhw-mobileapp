874a21100292053e7b96c802b4b72653244a3930
Prajwal W <prajwalwaingankar@gmail.com>
2021-10-23 17:43:49 +0530

    Cardview created and exapandable support added.

--- app/src/main/java/org/intelehealth/msfarogyabharat/activities/visitSummaryActivity/VisitSummaryAdapter.java
+++ app/src/main/java/org/intelehealth/msfarogyabharat/activities/visitSummaryActivity/VisitSummaryAdapter.java
@@ -3,6 +3,7 @@ package org.intelehealth.msfarogyabharat.activities.visitSummaryActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -41,6 +42,9 @@ public class VisitSummaryAdapter extends RecyclerView.Adapter<VisitSummaryAdapte
     public void onBindViewHolder(VisitSummaryAdapter.VisitSummaryViewHolder holder, int position) {
         String complaint = complaintList.get(position);
         String physexam = physexamList.get(position);
+      //  String visits = visitUuidList.get(position);
+
+        holder.textView_caseTitle.setText("Case " + (position + 1));
         holder.textView_content_complaint.setText(complaint);
         holder.textView_content_physexam.setText(physexam);
     }
@@ -51,12 +55,29 @@ public class VisitSummaryAdapter extends RecyclerView.Adapter<VisitSummaryAdapte
     }
 
     public class VisitSummaryViewHolder extends RecyclerView.ViewHolder {
-        TextView textView_content_complaint, textView_content_physexam;
+        TextView textView_caseTitle, textView_content_complaint, textView_content_physexam;
+        LinearLayout linearlayout_body;
 
         public VisitSummaryViewHolder(View itemView) {
             super(itemView);
+            textView_caseTitle = itemView.findViewById(R.id.textView_caseTitle);
             textView_content_complaint = itemView.findViewById(R.id.textView_content_complaint);
             textView_content_physexam = itemView.findViewById(R.id.textView_content_physexam);
+            linearlayout_body = itemView.findViewById(R.id.linearlayout_body);
+
+            textView_caseTitle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(linearlayout_body.getVisibility() == View.VISIBLE) {
+                        linearlayout_body.setVisibility(View.GONE);
+                    }
+                    else {
+                        linearlayout_body.setVisibility(View.VISIBLE);
+                    }
+
+
+                }
+            });
         }
     }
 }
