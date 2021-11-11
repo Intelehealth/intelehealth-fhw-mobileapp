d2e72695657121696f9d8e393ee4e59778263750
Prajwal <prajwalwaingankar@gmail.com>
2021-11-11 17:06:45 +0530

    link is added for prescription.

--- app/src/main/java/org/intelehealth/msfarogyabharat/activities/visitSummaryActivity/VisitSummaryActivity.java
+++ app/src/main/java/org/intelehealth/msfarogyabharat/activities/visitSummaryActivity/VisitSummaryActivity.java
@@ -302,6 +302,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
     private boolean allVisitsEnded = false;
     private boolean hide_endvisit = false;
     String shareoptionsarray[] = {"Whatsapp", "Email"};
+    public static final String prescriptionUrl = "https://www.training.vikalpindia.org/#/prescription/";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -573,7 +574,8 @@ public class VisitSummaryActivity extends AppCompatActivity {
 
                     // redirect to web browser for prescription
                 Intent intent1 = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://training.vikalpindia.org/intelehealth/index.html#/"));
+                        Uri.parse(prescriptionUrl + patientUuid));
+                Log.v("main", "prescurl: " +prescriptionUrl + patientUuid);
                 startActivity(intent1);
 
 /*                } catch (ParseException e) {
@@ -1941,9 +1943,10 @@ public class VisitSummaryActivity extends AppCompatActivity {
     }
 
     private void shareEmail() {
-        String to = "prajwalwaingankar@gmail.com";
-        String subject= "Share Presc";
-        String body="https://training.vikalpindia.org/intelehealth/index.html#/";
+        String to = "prajwal@intelehealth.org";
+        String subject = "Prescription";
+        String body = prescriptionUrl + patientUuid; //www.training.vikalpindia.org/#/prescription/patientId
+        Log.v("main", "prescurl: " +body);
         String mailTo = "mailto:" + to +
                 "?&subject=" + Uri.encode(subject) +
                 "&body=" + Uri.encode(body);
@@ -1953,7 +1956,8 @@ public class VisitSummaryActivity extends AppCompatActivity {
     }
 
     private void shareWhatsapp(String phoneNumberWithCountryCode) {
-        String message = "https://training.vikalpindia.org/intelehealth/index.html#/";
+        String message = prescriptionUrl + patientUuid;
+        Log.v("main", "prescurl: " +message);
         startActivity(new Intent(Intent.ACTION_VIEW,
                 Uri.parse(
                         String.format("https://api.whatsapp.com/send?phone=%s&text=%s",
