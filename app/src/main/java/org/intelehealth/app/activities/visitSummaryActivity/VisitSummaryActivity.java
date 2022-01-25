cb8fdf8fbef70ab3a9830dcd14781e4627fd1981
Ritika <ritika@intelehealth.org>
2022-01-25 23:44:51 +0530

    Add strings For all languages

--- app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
@@ -437,6 +437,8 @@ public class VisitSummaryActivity extends AppCompatActivity {
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.dismiss();
                     endVisit();
+                    AppointmentDAO appointmentDAO = new AppointmentDAO();
+                    appointmentDAO.deleteAppointmentByVisitId(visitUuid);
                 }
             });
             AlertDialog alertDialog = alertDialogBuilder.show();
