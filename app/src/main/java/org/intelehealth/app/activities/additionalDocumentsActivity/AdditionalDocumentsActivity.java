262330eecd302c264531bbf3f6d4ac1c75012ffa
DESKTOP-AFIF7ME\Admin <kuldeep@intelehealth.org>
2022-02-01 12:50:47 +0530

    additional document crash fixed

--- app/src/main/java/org/intelehealth/app/activities/additionalDocumentsActivity/AdditionalDocumentsActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/additionalDocumentsActivity/AdditionalDocumentsActivity.java
@@ -157,22 +157,23 @@ public class AdditionalDocumentsActivity extends AppCompatActivity {
             }
         }
         else if (requestCode == PICK_IMAGE_FROM_GALLERY) {
-            Uri selectedImage = data.getData();
-            String[] filePath = {MediaStore.Images.Media.DATA};
-            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
-            c.moveToFirst();
-            int columnIndex = c.getColumnIndex(filePath[0]);
-            String picturePath = c.getString(columnIndex);
-            c.close();
-            //Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-            Log.v("path", picturePath + "");
+            if(data!=null) {
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                //Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.v("path", picturePath + "");
 
-            // copy & rename the file
-            String finalImageName = UUID.randomUUID().toString();
-            final String finalFilePath = AppConstants.IMAGE_PATH + finalImageName + ".jpg";
-            BitmapUtils.copyFile(picturePath, finalFilePath);
-            compressImageAndSave(finalFilePath);
-
+                // copy & rename the file
+                String finalImageName = UUID.randomUUID().toString();
+                final String finalFilePath = AppConstants.IMAGE_PATH + finalImageName + ".jpg";
+                BitmapUtils.copyFile(picturePath, finalFilePath);
+                compressImageAndSave(finalFilePath);
+            }
         }
     }
     private void updateImageDatabase(String imageuuid) {
