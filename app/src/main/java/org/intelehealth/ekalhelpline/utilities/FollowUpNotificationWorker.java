1adda217455195de43c152d8cde939c25733fe91
Nishita Goyal <goyalnishita07@gmail.com>
2022-02-11 14:19:26 +0530

    Visit speciality issue fixed

--- app/src/main/java/org/intelehealth/ekalhelpline/utilities/FollowUpNotificationWorker.java
+++ app/src/main/java/org/intelehealth/ekalhelpline/utilities/FollowUpNotificationWorker.java
@@ -93,7 +93,7 @@ public class FollowUpNotificationWorker extends Worker {
         int count = 0;
         Date cDate = new Date();
         String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).format(cDate);
-        String query = "SELECT a.uuid, a.sync, a.patientuuid, a.startdate, a.enddate, b.uuid, b.first_name, b.middle_name, b.last_name, b.date_of_birth, b.openmrs_id, c.value AS speciality, o.value FROM tbl_visit a, tbl_patient b, tbl_encounter d, tbl_obs o, tbl_visit_attribute c WHERE a.uuid = c.visit_uuid AND  a.enddate is NOT NULL AND a.patientuuid = b.uuid AND a.uuid = d.visituuid AND d.uuid = o.encounteruuid AND d.provider_uuid = ? AND o.conceptuuid = ? AND o.value is NOT NULL GROUP BY a.patientuuid";
+        String query = "SELECT a.uuid, a.sync, a.patientuuid, a.startdate, a.enddate, b.uuid, b.first_name, b.middle_name, b.last_name, b.date_of_birth, b.openmrs_id, c.value AS speciality, o.value FROM tbl_visit a, tbl_patient b, tbl_encounter d, tbl_obs o, tbl_visit_attribute c WHERE a.uuid = c.visit_uuid AND  a.enddate is NOT NULL AND a.patientuuid = b.uuid AND a.uuid = d.visituuid AND c.visit_attribute_type_uuid = '3f296939-c6d3-4d2e-b8ca-d7f4bfd42c2d' AND d.uuid = o.encounteruuid AND d.provider_uuid = ? AND o.conceptuuid = ? AND o.value is NOT NULL GROUP BY a.patientuuid";
         final Cursor cursor = db.rawQuery(query, new String[]{chw_name, UuidDictionary.FOLLOW_UP_VISIT});
         if (cursor != null) {
             if (cursor.moveToFirst()) {
