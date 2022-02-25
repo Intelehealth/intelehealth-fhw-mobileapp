d72e45a7b323550aa7a92ee458ee991df4e5229b
Nishita Goyal <goyalnishita07@gmail.com>
2022-02-25 11:51:52 +0530

    Revert "Merge remote-tracking branch 'origin/Nashik-Arogya-Sampada-Master-UI-Fixes' into NAS-1_Validation_Fixes"
    This reverts commit 05f85c43115b28ef2d9ced2b1e047796e5113855.

--- app/src/main/java/org/intelehealth/app/activities/identificationActivity/HouseholdSurveyAdapter.java
+++ app/src/main/java/org/intelehealth/app/activities/identificationActivity/HouseholdSurveyAdapter.java
@@ -13,11 +13,9 @@ import java.util.List;
 public class HouseholdSurveyAdapter extends RecyclerView.Adapter<HouseholdSurveyAdapter.SurveyViewHolder> {
 
     private final List<HealthIssues> healthIssuesList;
-    private final ViewPagerCallback callback;
 
-    public HouseholdSurveyAdapter(List<HealthIssues> healthIssuesList, ViewPagerCallback callback) {
+    public HouseholdSurveyAdapter(List<HealthIssues> healthIssuesList) {
         this.healthIssuesList = healthIssuesList;
-        this.callback = callback;
     }
 
     @NonNull
@@ -28,12 +26,23 @@ public class HouseholdSurveyAdapter extends RecyclerView.Adapter<HouseholdSurvey
                 parent,
                 false
         );
-        return new SurveyViewHolder(binding, callback);
+
+        return new SurveyViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SurveyViewHolder holder, int position) {
-        holder.initData(healthIssuesList.get(position), position);
+//        holder.binding.nameOfHouseholdMemberValueTextView.setText(surveyPOJOList.get(position).getHouseholdMemberName());
+        holder.binding.healthIssueReportedValueTextView.setText(healthIssuesList.get(position).getHealthIssueReported());
+        holder.binding.numberOfIssuesEpisodesInTheLastYearValueTextView.setText(healthIssuesList.get(position).getNumberOfEpisodesInTheLastYear());
+        holder.binding.primaryHealthCareProviderValueTextView.setText(healthIssuesList.get(position).getPrimaryHealthcareProviderValue());
+        holder.binding.firstLocationOfVisitValueTextView.setText(healthIssuesList.get(position).getFirstLocationOfVisit());
+        holder.binding.referredToValueTextView.setText(healthIssuesList.get(position).getReferredTo());
+        holder.binding.modeOfTransportationValueTextView.setText(healthIssuesList.get(position).getModeOfTransportation());
+        holder.binding.averageCostIncurredOnTravelAndStayValueTextView.setText(healthIssuesList.get(position).getAverageCostOfTravelAndStayPerEpisode());
+        holder.binding.averageCostIncurredOnConsultationFeesValueTextView.setText(healthIssuesList.get(position).getAverageCostOfConsultation());
+        holder.binding.averageCostIncurredOnMedicinesValueTextView.setText(healthIssuesList.get(position).getAverageCostOfMedicine());
+        holder.binding.scoreOfExperienceValueTextView.setText(healthIssuesList.get(position).getScoreForExperienceOfTreatment());
     }
 
     @Override
@@ -43,30 +52,10 @@ public class HouseholdSurveyAdapter extends RecyclerView.Adapter<HouseholdSurvey
 
     static class SurveyViewHolder extends RecyclerView.ViewHolder {
         public LayoutDiseasePresentationBinding binding;
-        public HealthIssues healthIssues;
-        public ViewPagerCallback callback;
-        public int position;
 
-        public SurveyViewHolder(@NonNull LayoutDiseasePresentationBinding binding, ViewPagerCallback viewPagerCallback) {
+        public SurveyViewHolder(@NonNull LayoutDiseasePresentationBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            this.callback = viewPagerCallback;
-            binding.linearLayout.setOnClickListener(v -> this.callback.getIssueClicked(healthIssues, position));
-        }
-
-        public void initData(HealthIssues issues, int position) {
-            this.healthIssues = issues;
-            this.position = position;
-            binding.healthIssueReportedValueTextView.setText(issues.getHealthIssueReported());
-            binding.numberOfIssuesEpisodesInTheLastYearValueTextView.setText(issues.getNumberOfEpisodesInTheLastYear());
-            binding.primaryHealthCareProviderValueTextView.setText(issues.getPrimaryHealthcareProviderValue());
-            binding.firstLocationOfVisitValueTextView.setText(issues.getFirstLocationOfVisit());
-            binding.referredToValueTextView.setText(issues.getReferredTo());
-            binding.modeOfTransportationValueTextView.setText(issues.getModeOfTransportation());
-            binding.averageCostIncurredOnTravelAndStayValueTextView.setText(issues.getAverageCostOfTravelAndStayPerEpisode());
-            binding.averageCostIncurredOnConsultationFeesValueTextView.setText(issues.getAverageCostOfConsultation());
-            binding.averageCostIncurredOnMedicinesValueTextView.setText(issues.getAverageCostOfMedicine());
-            binding.scoreOfExperienceValueTextView.setText(issues.getScoreForExperienceOfTreatment());
         }
     }
 }
--- app/src/main/java/org/intelehealth/app/activities/identificationActivity/ViewPagerCallback.java
+++
@@ -1,5 +0,0 @@
-package org.intelehealth.app.activities.identificationActivity;
-
-interface ViewPagerCallback {
-    void getIssueClicked(HealthIssues survey, int position);
-}
