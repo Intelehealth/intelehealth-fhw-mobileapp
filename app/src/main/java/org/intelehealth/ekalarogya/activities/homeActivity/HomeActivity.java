2c150733a1106f217a748bf42dbdc8332122dfce
DESKTOP-AFIF7ME\Admin <kuldeep@intelehealth.org>
2022-03-04 13:44:03 +0530

    home screen ui changed

--- app/src/main/java/org/intelehealth/ekalarogya/activities/homeActivity/HomeActivity.java
+++ app/src/main/java/org/intelehealth/ekalarogya/activities/homeActivity/HomeActivity.java
@@ -108,7 +108,7 @@ public class HomeActivity extends AppCompatActivity {
 
     TextView lastSyncTextView;
     TextView lastSyncAgo;
-    Button manualSyncButton;
+    CardView manualSyncButton;
     //IntentFilter filter;
 
     SyncUtils syncUtils = new SyncUtils();
@@ -189,7 +189,7 @@ public class HomeActivity extends AppCompatActivity {
         help_textview.setText(R.string.Whatsapp_Help_Cardview);
 
         // manualSyncButton.setText(R.string.sync_now);
-        manualSyncButton.setText(R.string.refresh);
+        //manualSyncButton.setText(R.string.refresh);
 
         //Help section of watsapp...
         c6.setOnClickListener(new View.OnClickListener() {
