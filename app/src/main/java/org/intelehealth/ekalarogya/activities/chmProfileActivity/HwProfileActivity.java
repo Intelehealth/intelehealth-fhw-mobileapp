80d1bce9225c48a4911847b5604341778ddce005
DESKTOP-AFIF7ME\Admin <kuldeep@intelehealth.org>
2022-03-08 17:43:02 +0530

    hw profile picture change ui part completed api implementation pending

--- app/src/main/java/org/intelehealth/ekalarogya/activities/chmProfileActivity/HwProfileActivity.java
+++ app/src/main/java/org/intelehealth/ekalarogya/activities/chmProfileActivity/HwProfileActivity.java
@@ -6,26 +6,36 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
 import org.intelehealth.ekalarogya.R;
+import org.intelehealth.ekalarogya.activities.cameraActivity.CameraActivity;
 import org.intelehealth.ekalarogya.activities.patientDetailActivity.PatientDetailActivity;
 import org.intelehealth.ekalarogya.app.AppConstants;
 import org.intelehealth.ekalarogya.database.dao.ImagesDAO;
 import org.intelehealth.ekalarogya.database.dao.PatientsDAO;
+import org.intelehealth.ekalarogya.models.DocumentObject;
 import org.intelehealth.ekalarogya.models.UserProfileModel.HwPersonalInformationModel;
 import org.intelehealth.ekalarogya.models.UserProfileModel.HwProfileModel;
 import org.intelehealth.ekalarogya.models.UserProfileModel.MainProfileModel;
+import org.intelehealth.ekalarogya.services.DownloadProtocolsTask;
 import org.intelehealth.ekalarogya.utilities.DownloadFilesUtils;
 import org.intelehealth.ekalarogya.utilities.Logger;
 import org.intelehealth.ekalarogya.utilities.NetworkConnection;
 import org.intelehealth.ekalarogya.utilities.SessionManager;
+import org.intelehealth.ekalarogya.utilities.StringUtils;
 import org.intelehealth.ekalarogya.utilities.UrlModifiers;
+import org.intelehealth.ekalarogya.utilities.UuidDictionary;
 import org.intelehealth.ekalarogya.utilities.exception.DAOException;
 
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -45,7 +55,9 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.google.gson.Gson;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
+import java.io.File;
 import java.util.Locale;
+import java.util.UUID;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -54,11 +66,14 @@ import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 
 public class HwProfileActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_FROM_GALLERY = 2001;
     SessionManager sessionManager = null;
     TextView hw_name_value, hw_designation_value, total_patregistered_value, total_visitprogress_value,
             total_consultaion_value, hw_gender_value, hw_state_value, hw_mobile_value,
             hw_whatsapp_value, hw_email_value, hw_aboutme_value;
     CircularImageView hw_profile_image;
+    private DownloadProtocolsTask BitmapUtils;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -156,6 +171,7 @@ public class HwProfileActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_today_patient, menu);
+        inflater.inflate(R.menu.menu_edit_hw_profile, menu);
         inflater.inflate(R.menu.today_filter, menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -167,9 +183,38 @@ public class HwProfileActivity extends AppCompatActivity {
                 finish();
                 return true;
 
+            case R.id.hw_profile_image_edit:
+               selectImage();
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void selectImage(){
+        final CharSequence[] options = {getString(R.string.take_photo), getString(R.string.choose_from_gallery), getString(R.string.cancel)};
+        AlertDialog.Builder builder = new AlertDialog.Builder(HwProfileActivity.this);
+        builder.setTitle(R.string.hw_profile_image_picker_title);
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (item == 0) {
+                    Intent cameraIntent = new Intent(HwProfileActivity.this, CameraActivity.class);
+                    String imageName = UUID.randomUUID().toString();
+                    cameraIntent.putExtra(CameraActivity.SET_IMAGE_NAME, imageName);
+                    cameraIntent.putExtra(CameraActivity.SET_IMAGE_PATH, AppConstants.IMAGE_PATH);
+                    startActivityForResult(cameraIntent, CameraActivity.TAKE_IMAGE);
+
+                } else if (item == 1) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, PICK_IMAGE_FROM_GALLERY);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 
     public void DisplayUserDetail(){
@@ -208,6 +253,44 @@ public class HwProfileActivity extends AppCompatActivity {
             hw_email_value.setText(personalInformationModel.getEmail());
         }else{
             Toast.makeText(HwProfileActivity.this, HwProfileActivity.this.getString(R.string.please_connect_to_internet), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CameraActivity.TAKE_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                String mCurrentPhotoPath = data.getStringExtra("RESULT");
+                File photo = new File(mCurrentPhotoPath);
+                Glide.with(HwProfileActivity.this)
+                        .load(photo)
+                        .thumbnail(0.3f)
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .into(hw_profile_image);
+            }
+        } else if (requestCode == PICK_IMAGE_FROM_GALLERY) {
+            if (resultCode == RESULT_OK && null != data) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                File file=new File(picturePath);
+                Glide.with(HwProfileActivity.this)
+                        .load(file)
+                        .thumbnail(0.3f)
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .into(hw_profile_image);
+                // String picturePath contains the path of selected Image
+            }
         }
     }
 }
