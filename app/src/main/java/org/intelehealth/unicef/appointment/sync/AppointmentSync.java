2ff1648ed8fc369b0b47b760abacd06d18652139
Nishita Goyal <goyalnishita07@gmail.com>
2022-04-26 15:34:59 +0530

    Firebase Crashlytics fixed for UK-193

--- app/src/main/java/org/intelehealth/unicef/appointment/sync/AppointmentSync.java
+++ app/src/main/java/org/intelehealth/unicef/appointment/sync/AppointmentSync.java
@@ -3,6 +3,7 @@ package org.intelehealth.unicef.appointment.sync;
 import android.content.Context;
 import android.util.Log;
 
+import org.intelehealth.unicef.app.IntelehealthApplication;
 import org.intelehealth.unicef.appointment.api.ApiClientAppointment;
 import org.intelehealth.unicef.appointment.dao.AppointmentDAO;
 import org.intelehealth.unicef.appointment.model.AppointmentListingResponse;
@@ -18,10 +19,25 @@ import retrofit2.Callback;
 
 public class AppointmentSync {
     public static void getAppointments(Context context) {
+        SessionManager sessionManager = new SessionManager(IntelehealthApplication.getAppContext());
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
         String selectedStartDate = simpleDateFormat.format(new Date());
         String selectedEndDate = simpleDateFormat.format(new Date(new Date().getTime() + 30L * 24 * 60 * 60 * 1000));
-        String baseurl = "https://" + new SessionManager(context).getServerUrl() + ":3004";
+//        String baseurl = "https://" + new SessionManager(context).getServerUrl() + ":3004";
+
+        /* below changes are done to handle the following crash in firebase crashlytics
+            Fatal Exception: java.lang.RuntimeException
+            Unable to start activity ComponentInfo{org.intelehealth.unicef/org.intelehealth.unicef.activities.homeActivity.HomeActivity}:
+            java.lang.IllegalArgumentException: Invalid URL host: ""
+             Version No. 1.8.13(23)
+
+            - By Nishita Goyal on 26th April 2022
+         */
+
+        String baseurl = "";
+        if (sessionManager.getServerUrl() != null && !sessionManager.getServerUrl().equalsIgnoreCase(""))
+            baseurl = "https://" + sessionManager.getServerUrl() + ":3004";
+        else return;
         ApiClientAppointment.getInstance(baseurl).getApi()
                 .getSlotsAll(selectedStartDate, selectedEndDate, new SessionManager(context).getLocationUuid())
 
