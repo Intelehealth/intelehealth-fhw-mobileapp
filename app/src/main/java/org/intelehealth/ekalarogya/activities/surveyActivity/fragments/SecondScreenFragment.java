e422b85fb43084596bc54813b1dd6ded5574e794
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2022-05-11 11:41:42 +0530

    Classes created for all the survey screens

---
+++ app/src/main/java/org/intelehealth/ekalarogya/activities/surveyActivity/fragments/SecondScreenFragment.java
@@ -0,0 +1,21 @@
+package org.intelehealth.ekalarogya.activities.surveyActivity.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.intelehealth.ekalarogya.R;
+
+public class SecondScreenFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_second_screen, container, false);
+        return rootView;
+    }
+}
