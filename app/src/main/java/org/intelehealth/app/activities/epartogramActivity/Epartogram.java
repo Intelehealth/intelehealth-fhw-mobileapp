5c37cec6c05f896d3970586434a56f0b049a2d20
Prajwal <prajwalwaingankar@gmail.com>
2022-06-07 14:51:30 +0530

    Webview progress circle added.

--- app/src/main/java/org/intelehealth/app/activities/epartogramActivity/Epartogram.java
+++ app/src/main/java/org/intelehealth/app/activities/epartogramActivity/Epartogram.java
@@ -3,14 +3,20 @@ package org.intelehealth.app.activities.epartogramActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewTreeObserver;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.intelehealth.app.R;
+import org.intelehealth.app.activities.IntroActivity.IntroActivity;
+import org.intelehealth.app.widget.materialprogressbar.CustomProgressDialog;
 
 public class Epartogram extends AppCompatActivity {
     WebView webView;
@@ -20,6 +26,7 @@ public class Epartogram extends AppCompatActivity {
     // "df07db0d-d9b9-4597-a9e5-d62d3cff3d45/705397d4-0c62-4f26-bd53-2dd8523d5d1b";
     private SwipeRefreshLayout mySwipeRefreshLayout;
     private ViewTreeObserver.OnScrollChangedListener mOnScrollChangedListener;
+    CustomProgressDialog customProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +42,33 @@ public class Epartogram extends AppCompatActivity {
 
         webView = findViewById(R.id.webview_epartogram);
         mySwipeRefreshLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipeContainer);
+        customProgressDialog = new CustomProgressDialog(Epartogram.this);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(true);
-//        webView.setInitialScale(1);
-//        webView.getSettings().setLoadWithOverviewMode(true);
-//        webView.getSettings().setUseWideViewPort(true);
-
         webView.setWebViewClient(new WebViewClient() {
+
             @Override
             public void onPageFinished(WebView view, String url) {
                 mySwipeRefreshLayout.setRefreshing(false);
+                if (customProgressDialog.isShowing()) {
+                    customProgressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if (customProgressDialog.isShowing()) {
+                    customProgressDialog.dismiss();
+                }
             }
         });
+
+        customProgressDialog.show();
         webView.loadUrl(URL + patientUuid + "/" + visitUuid);
-
-
+        
         mySwipeRefreshLayout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
