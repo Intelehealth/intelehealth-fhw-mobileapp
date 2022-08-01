a4365515dddf9c26207a5aeddf89f08f2991f5d7
Kaveri <kaveri@intelehealth.org>
2022-08-01 15:05:34 +0530

    Login UI - IDA New Version

---
+++ app/src/main/java/org/intelehealth/app/activities/LoginActivityNew.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.intelehealth.app.R;
+
+public class LoginActivityNew extends AppCompatActivity {
+    private static final String TAG = "LoginActivityNew";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_new);
+    }
+}
