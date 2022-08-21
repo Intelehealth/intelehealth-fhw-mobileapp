3161223a40cab34ca423acbb544d7bdc891a3771
Prajwal <prajwalwaingankar@gmail.com>
2022-08-22 02:12:23 +0530

    followup visit details screen design.

---
+++ app/src/main/java/org/intelehealth/app/activities/FollowUp_VisitDetails.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.intelehealth.app.R;
+
+public class FollowUp_VisitDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow_up_visit_details);
+    }
+}
