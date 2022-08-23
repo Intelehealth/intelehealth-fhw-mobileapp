711387e0dd11800c7c2efee720e1afe0bce03633
Kaveri <kaveri@intelehealth.org>
2022-08-23 19:07:15 +0530

    Intro screens and ayu demo first screen  - In progress

---
+++ app/src/main/java/org/intelehealth/app/ui2/onboarding/SetupPrivacyNoteActivity.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.ui2.onboarding;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.intelehealth.app.R;
+
+public class SetupPrivacyNoteActivity extends AppCompatActivity {
+    private static final String TAG = "SetupPrivacyNoteActivit";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_privacy_note_ui2);
+    }
+}
