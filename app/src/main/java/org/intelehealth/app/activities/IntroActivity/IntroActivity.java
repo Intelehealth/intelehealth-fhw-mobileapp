20814b03a8a8a227009e707c46fe9c70da245828
Nishita Goyal <goyalnishita07@gmail.com>
2022-09-26 14:20:47 +0530

    Demo version location changes done

--- app/src/main/java/org/intelehealth/app/activities/IntroActivity/IntroActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/IntroActivity/IntroActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.accounts.Account;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
@@ -32,16 +33,21 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.intelehealth.app.R;
 import org.intelehealth.app.activities.homeActivity.HomeActivity;
+import org.intelehealth.app.activities.setupActivity.LocationArrayAdapter;
 import org.intelehealth.app.activities.setupActivity.SetupActivity;
 import org.intelehealth.app.app.AppConstants;
+import org.intelehealth.app.database.dao.NewLocationDao;
 import org.intelehealth.app.models.Location;
 import org.intelehealth.app.models.Results;
 import org.intelehealth.app.models.loginModel.LoginModel;
 import org.intelehealth.app.models.loginProviderModel.LoginProviderModel;
+import org.intelehealth.app.models.statewise_location.Setup_LocationModel;
 import org.intelehealth.app.networkApiCalls.ApiClient;
 import org.intelehealth.app.networkApiCalls.ApiInterface;
 import org.intelehealth.app.utilities.AdminPassword;
@@ -52,6 +58,7 @@ import org.intelehealth.app.utilities.Logger;
 import org.intelehealth.app.utilities.SessionManager;
 import org.intelehealth.app.utilities.StringEncryption;
 import org.intelehealth.app.utilities.UrlModifiers;
+import org.intelehealth.app.utilities.exception.DAOException;
 import org.intelehealth.apprtc.data.Manager;
 
 import io.reactivex.Observable;
@@ -81,6 +88,8 @@ public class IntroActivity extends AppCompatActivity {
     SessionManager sessionManager = null;
     String BASE_URL = "";
     private long createdRecordsCount = 0;
+    Map.Entry<String, String> village_name;
+    private String selectedState = "", selectedDistrict = "", selectedVillage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,7 +99,7 @@ public class IntroActivity extends AppCompatActivity {
         context = IntroActivity.this;
         sessionManager = new SessionManager(this);
 
-        BASE_URL = "https://demo.intelehealth.org/openmrs/ws/rest/v1/";
+        BASE_URL = "https://syrianatest.intelehealth.org:3004/api/openmrs/";
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
@@ -239,7 +248,6 @@ public class IntroActivity extends AppCompatActivity {
                 button_demo_visit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
                         progress = new ProgressDialog(IntroActivity.this, R.style.AlertDialogStyle);
                         progress.setTitle(getString(R.string.please_wait_progress));
                         progress.setMessage(getString(R.string.logging_in));
@@ -286,22 +294,42 @@ public class IntroActivity extends AppCompatActivity {
         ApiClient.changeApiBaseUrl(url);
         ApiInterface apiService = ApiClient.createService(ApiInterface.class);
         try {
-            Observable<Results<Location>> resultsObservable = apiService.LOCATION_OBSERVABLE(null);
+            Observable<Setup_LocationModel> resultsObservable = apiService.SETUP_LOCATIONOBSERVABLE();
             resultsObservable
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new DisposableObserver<Results<Location>>() {
+                    .subscribe(new DisposableObserver<Setup_LocationModel>() {
                         @Override
-                        public void onNext(Results<Location> locationResults) {
-                            if (locationResults.getResults() != null) {
-                                Results<Location> locationList = locationResults;
-                                mLocations = locationList.getResults();
-                                if (mLocations.size() > 1)
-                                    location = mLocations.get(1);
-                                else
-                                    location = mLocations.get(0);
+                        public void onNext(Setup_LocationModel location) {
+                            if (location.getStates() != null) {
+                                try {
+                                    NewLocationDao newLocationDao = new NewLocationDao();
+                                    newLocationDao.insertSetupLocations(location);
+                                    List<String> state_locations = newLocationDao.getStateList(context);
+                                    if (state_locations.size() != 0 && state_locations.size()>1) {
+                                        selectedState = state_locations.get(1);
+                                        List<String> district_locations = newLocationDao.getDistrictList(state_locations.get(1), context);
+                                        if (district_locations.size() != 0 && district_locations.size()>1) {
+                                            selectedDistrict = district_locations.get(1);
+                                            List<String> village_locations = newLocationDao.getVillageList(state_locations.get(1), district_locations.get(1), context);
+                                            if (village_locations.size() != 0 && village_locations.size()>1) {
+                                                selectedVillage = village_locations.get(1);
+                                                HashMap<String, String> hashMap4;
+                                                String village_uuid = newLocationDao.getVillageUuid(state_locations.get(1), district_locations.get(1), village_locations.get(1));
+                                                hashMap4 = new HashMap<>();
+                                                hashMap4.put(village_uuid, village_locations.get(1));
+                                                for (Map.Entry<String, String> entry : hashMap4.entrySet()) {
+                                                    village_name = entry;
+                                                }
+                                                TestSetup("syrianatest.intelehealth.org", "nurse1", "Nurse123", "", village_name);
+                                            }
+                                        }
+                                    } else {
+                                    }
 
-                                TestSetup("demo.intelehealth.org", "nurse1", "Nurse123", "", location);
+                                } catch (DAOException e) {
+                                    e.printStackTrace();
+                                }
 
                             } else {
                                 progress.dismiss();
@@ -334,7 +362,7 @@ public class IntroActivity extends AppCompatActivity {
         return list;
     }
 
-    public void TestSetup(String CLEAN_URL, String USERNAME, String PASSWORD, String ADMIN_PASSWORD, Location location) {
+    /*public void TestSetup(String CLEAN_URL, String USERNAME, String PASSWORD, String ADMIN_PASSWORD, Location location) {
 
         String urlString = urlModifiers.loginUrl(CLEAN_URL);
         Logger.logD(TAG, "usernaem and password" + USERNAME + PASSWORD);
@@ -373,8 +401,8 @@ public class IntroActivity extends AppCompatActivity {
                                             Log.i(TAG, "doInBackground: " + loginProviderModel.getResults().get(i).getUuid());
                                             sessionManager.setProviderID(loginProviderModel.getResults().get(i).getUuid());
 //                                                responsecode = 200;
-                                          /*  final Account account = new Account(USERNAME, "io.intelehealth.openmrs");
-                                            manager.addAccountExplicitly(account, PASSWORD, null);*/
+//                                           final Account account = new Account(USERNAME, "io.intelehealth.openmrs");
+//                                            manager.addAccountExplicitly(account, PASSWORD, null);
 
                                             sessionManager.setLocationName(location.getDisplay());
                                             sessionManager.setLocationUuid(location.getUuid());
@@ -469,6 +497,161 @@ public class IntroActivity extends AppCompatActivity {
                 progress.dismiss();
                 DialogUtils dialogUtils = new DialogUtils();
                 dialogUtils.showerrorDialog(IntroActivity.this, "Error Login", getString(R.string.error_incorrect_password), "ok");
+            }
+
+            @Override
+            public void onComplete() {
+                Logger.logD(TAG, "completed");
+            }
+        });
+
+
+    }*/
+
+
+    /**
+     * Attempts login to the OpenMRS server.
+     * If successful cretes a new {@link Account}
+     * If unsuccessful details are saved in SharedPreferences.
+     */
+    public void TestSetup(String CLEAN_URL, String USERNAME, String PASSWORD, String ADMIN_PASSWORD, Map.Entry<String, String> location) {
+
+        String urlString = urlModifiers.loginUrl(CLEAN_URL);
+        Logger.logD(TAG, "usernaem and password" + USERNAME + PASSWORD);
+        encoded = base64Utils.encoded(USERNAME, PASSWORD);
+        sessionManager.setEncoded(encoded);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Observable<LoginModel> loginModelObservable = AppConstants.apiInterface.LOGIN_MODEL_OBSERVABLE(urlString, "Basic " + encoded);
+        loginModelObservable.subscribe(new Observer<LoginModel>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(LoginModel loginModel) {
+                Boolean authencated = loginModel.getAuthenticated();
+                Gson gson = new Gson();
+                sessionManager.setChwname(loginModel.getUser().getDisplay());
+                sessionManager.setCreatorID(loginModel.getUser().getUuid());
+                sessionManager.setSessionID(loginModel.getSessionId());
+                sessionManager.setProviderID(loginModel.getUser().getPerson().getUuid());
+                UrlModifiers urlModifiers = new UrlModifiers();
+                String url = urlModifiers.loginUrlProvider(CLEAN_URL, loginModel.getUser().getUuid());
+                if (authencated) {
+                    Observable<LoginProviderModel> loginProviderModelObservable = AppConstants.apiInterface.LOGIN_PROVIDER_MODEL_OBSERVABLE(url, "Basic " + encoded);
+                    loginProviderModelObservable
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(new DisposableObserver<LoginProviderModel>() {
+                                @Override
+                                public void onNext(LoginProviderModel loginProviderModel) {
+                                    if (loginProviderModel.getResults().size() != 0) {
+                                        for (int i = 0; i < loginProviderModel.getResults().size(); i++) {
+                                            Log.i(TAG, "doInBackground: " + loginProviderModel.getResults().get(i).getUuid());
+                                            sessionManager.setProviderID(loginProviderModel.getResults().get(i).getUuid());
+//                                                responsecode = 200;
+                                          /*  final Account account = new Account(USERNAME, "io.intelehealth.openmrs");
+                                            manager.addAccountExplicitly(account, PASSWORD, null);*/
+
+                                            sessionManager.setLocationName(location.getValue());
+                                            sessionManager.setLocationUuid(location.getKey());
+                                            //  sessionManager.setLocationDescription(location.getDescription());
+                                            sessionManager.setServerUrl(CLEAN_URL);
+                                            sessionManager.setServerUrlRest(BASE_URL);
+                                            sessionManager.setServerUrlBase("https://" + CLEAN_URL + "/openmrs");
+                                            sessionManager.setBaseUrl(BASE_URL);
+                                            sessionManager.setSetupComplete(true);
+
+                                            //Storing State Name
+                                            sessionManager.setCountryName("Syria");
+                                            sessionManager.setStateName(selectedState);
+                                            sessionManager.setDistrictName(selectedDistrict);
+                                            sessionManager.setVillageName(selectedVillage);
+
+                                            // OfflineLogin.getOfflineLogin().setUpOfflineLogin(USERNAME, PASSWORD);
+                                            AdminPassword.getAdminPassword().setUp(ADMIN_PASSWORD);
+
+                                            Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                                                    .applicationId(AppConstants.IMAGE_APP_ID)
+                                                    .server("https://" + CLEAN_URL + ":1337/parse/")
+                                                    .build()
+                                            );
+
+                                            SQLiteDatabase sqLiteDatabase = AppConstants.inteleHealthDatabaseHelper.getWriteDb();
+                                            //SQLiteDatabase read_db = AppConstants.inteleHealthDatabaseHelper.getReadableDatabase();
+
+                                            sqLiteDatabase.beginTransaction();
+                                            //read_db.beginTransaction();
+                                            ContentValues values = new ContentValues();
+
+                                            //StringEncryption stringEncryption = new StringEncryption();
+                                            String random_salt = getSalt_DATA();
+
+                                            //String random_salt = stringEncryption.getRandomSaltString();
+                                            Log.d("salt", "salt: " + random_salt);
+                                            //Salt_Getter_Setter salt_getter_setter = new Salt_Getter_Setter();
+                                            //salt_getter_setter.setSalt(random`_salt);
+
+
+                                            String hash_password = null;
+                                            try {
+                                                //hash_email = StringEncryption.convertToSHA256(random_salt + mEmail);
+                                                hash_password = StringEncryption.convertToSHA256(random_salt + PASSWORD);
+                                            } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+                                                FirebaseCrashlytics.getInstance().recordException(e);
+                                            }
+
+                                            try {
+                                                values.put("username", USERNAME);
+                                                values.put("password", hash_password);
+                                                values.put("creator_uuid_cred", loginModel.getUser().getUuid());
+                                                values.put("chwname", loginModel.getUser().getDisplay());
+                                                values.put("provider_uuid_cred", sessionManager.getProviderID());
+                                                createdRecordsCount = sqLiteDatabase.insertWithOnConflict("tbl_user_credentials", null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                                                sqLiteDatabase.setTransactionSuccessful();
+
+                                                Logger.logD("values", "values" + values);
+                                                Logger.logD("created user credentials", "create user records" + createdRecordsCount);
+                                            } catch (SQLException e) {
+                                                Log.d("SQL", "SQL user credentials: " + e);
+                                            } finally {
+                                                sqLiteDatabase.endTransaction();
+                                            }
+
+                                            Log.i(TAG, "onPostExecute: Parse init");
+                                            Intent intent = new Intent(IntroActivity.this, HomeActivity.class);
+                                            intent.putExtra("setup", true);
+                                            sessionManager.setTriggerNoti("no");
+                                            startActivity(intent);
+                                            finish();
+                                            progress.dismiss();
+                                        }
+                                    }
+
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+                                    Logger.logD(TAG, "handle provider error" + e.getMessage());
+                                    progress.dismiss();
+                                }
+
+                                @Override
+                                public void onComplete() {
+
+                                }
+                            });
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Logger.logD(TAG, "Login Failure" + e.getMessage());
+                progress.dismiss();
             }
 
             @Override
