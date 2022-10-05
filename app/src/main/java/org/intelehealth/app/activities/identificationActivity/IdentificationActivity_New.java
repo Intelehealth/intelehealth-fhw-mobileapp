9c1ca15d7ec0cb9b7684bed30c49a31465891bbe
Prajwal <prajwalwaingankar@gmail.com>
2022-10-05 18:02:33 +0530

    Patient registration second screen done.

--- app/src/main/java/org/intelehealth/app/activities/identificationActivity/IdentificationActivity_New.java
+++ app/src/main/java/org/intelehealth/app/activities/identificationActivity/IdentificationActivity_New.java
@@ -2,7 +2,10 @@ package org.intelehealth.app.activities.identificationActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -18,6 +21,12 @@ public class IdentificationActivity_New extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identification_new);
+
+        // changing status bar color
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
 
       /*  getSupportFragmentManager().beginTransaction()
                 .replace(R.id.frame_firstscreen, new HomeFragment_New())
