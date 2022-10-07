09e2ccf3267ab931edc4e16cab89006580f0bd44
Prajwal <prajwalwaingankar@gmail.com>
2022-10-07 17:58:29 +0530

    Patient detail screen UI changes.

---
+++ app/src/main/java/org/intelehealth/app/activities/patientDetailActivity/PatientDetailActivity2.java
@@ -0,0 +1,20 @@
+package org.intelehealth.app.activities.patientDetailActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.intelehealth.app.R;
+
+public class PatientDetailActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_detail2);
+
+
+    }
+}
