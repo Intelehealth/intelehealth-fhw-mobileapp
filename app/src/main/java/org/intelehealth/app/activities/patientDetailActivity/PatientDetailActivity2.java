0a044e8bdd71fcf265755d05604531b6f4c5f0fd
Prajwal <prajwalwaingankar@gmail.com>
2022-10-10 16:25:17 +0530

    Patient Detail screen Mobile UI is done.

--- app/src/main/java/org/intelehealth/app/activities/patientDetailActivity/PatientDetailActivity2.java
+++ app/src/main/java/org/intelehealth/app/activities/patientDetailActivity/PatientDetailActivity2.java
@@ -3,7 +3,10 @@ package org.intelehealth.app.activities.patientDetailActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.intelehealth.app.R;
@@ -15,6 +18,11 @@ public class PatientDetailActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_detail2);
 
+        // changing status bar color
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
 
     }
 }
