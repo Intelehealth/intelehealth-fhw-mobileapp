8c60522cad8a295f50192c314adec51c71c853d3
Prajwal <prajwalwaingankar@gmail.com>
2022-11-03 12:28:21 +0530

    Prescription card added.

---
+++ app/src/main/java/org/intelehealth/app/activities/visit/PrescriptionActivity.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.activities.visit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.intelehealth.app.R;
+
+public class PrescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescription2);
+    }
+}
