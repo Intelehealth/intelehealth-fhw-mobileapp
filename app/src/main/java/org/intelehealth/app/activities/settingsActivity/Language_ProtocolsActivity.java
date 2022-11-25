0af35ad28a5a393d7fd22226a0cab615c19d9429
Prajwal <prajwalwaingankar@gmail.com>
2022-11-25 13:37:38 +0530

    Language and Protocols mobile UI done.

---
+++ app/src/main/java/org/intelehealth/app/activities/settingsActivity/Language_ProtocolsActivity.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.activities.settingsActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.intelehealth.app.R;
+
+public class Language_ProtocolsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_protocols);
+    }
+}
