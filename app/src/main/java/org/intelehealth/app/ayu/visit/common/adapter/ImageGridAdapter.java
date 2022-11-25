c7d607005d62f0fb7de3dc16739ef2ce77c1d303
Lincon Pradhan <i.can.do.it.now.for.you@gmail.com>
2022-11-25 10:06:20 +0530

    Image process issue fixed and medical history capturing in progress

--- app/src/main/java/org/intelehealth/app/ayu/visit/common/adapter/ImageGridAdapter.java
+++ app/src/main/java/org/intelehealth/app/ayu/visit/common/adapter/ImageGridAdapter.java
@@ -1,6 +1,9 @@
 package org.intelehealth.app.ayu.visit.common.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +12,16 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
 import org.intelehealth.app.R;
+import org.intelehealth.app.activities.cameraActivity.CameraActivity;
+import org.intelehealth.app.app.AppConstants;
 import org.intelehealth.app.knowledgeEngine.Node;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +31,7 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     private static final int TYPE_ITEM = 1;
     private static final int TYPE_FOOTER = 2;
     private Context mContext;
-    private List<Node> mItemList = new ArrayList<Node>();
+    private List<String> mItemList = new ArrayList<String>();
 
     public interface OnItemSelection {
         public void onSelect(Node data);
@@ -30,7 +39,7 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     private OnItemSelection mOnItemSelection;
 
-    public ImageGridAdapter(RecyclerView recyclerView, Context context, List<Node> itemList, OnItemSelection onItemSelection) {
+    public ImageGridAdapter(RecyclerView recyclerView, Context context, List<String> itemList, OnItemSelection onItemSelection) {
         mContext = context;
         mItemList = itemList;
         mOnItemSelection = onItemSelection;
@@ -57,9 +66,14 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if (holder instanceof GenericViewHolder) {
             GenericViewHolder genericViewHolder = (GenericViewHolder) holder;
-            genericViewHolder.node = mItemList.get(position);
+            genericViewHolder.image = mItemList.get(position);
             genericViewHolder.index = position;
-
+            Glide.with(mContext)
+                    .load(genericViewHolder.image)
+                    .skipMemoryCache(true)
+                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .thumbnail(0.1f)
+                    .into(genericViewHolder.mainImageView);
 
         }
     }
@@ -71,7 +85,7 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     private class GenericViewHolder extends RecyclerView.ViewHolder {
         ImageView mainImageView, addImageView, crossImageView;
-        Node node;
+        String image;
         int index;
 
         GenericViewHolder(View itemView) {
@@ -82,6 +96,7 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
                 @Override
                 public void onClick(View view) {
                    // start image capture activity
+                    openCamera(getImagePath(), image);
                 }
             });
 
@@ -90,6 +105,24 @@ public class ImageGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     }
 
+    private String getImagePath() {
+        File filePath = new File(AppConstants.IMAGE_PATH);
+        return filePath.getAbsolutePath();
+    }
+
+    public void openCamera(String imagePath, String imageName) {
+
+        Intent cameraIntent = new Intent(mContext, CameraActivity.class);
+        if (imageName != null && imagePath != null) {
+            File filePath = new File(imagePath);
+            if (!filePath.exists()) {
+                boolean res = filePath.mkdirs();
+            }
+            cameraIntent.putExtra(CameraActivity.SET_IMAGE_NAME, imageName);
+            cameraIntent.putExtra(CameraActivity.SET_IMAGE_PATH, imagePath);
+        }
+        mContext.startActivityForResult(cameraIntent, Node.TAKE_IMAGE_FOR_NODE);
+    }
 
 }
 
