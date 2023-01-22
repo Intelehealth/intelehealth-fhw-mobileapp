de1fa156b25a0424824c1f29246a5faea2b391b0
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-01-22 13:11:29 +0530

    Implemented query for calculating the average patient satisfaction score for the current date

--- app/src/main/java/org/intelehealth/app/activities/achievements/fragments/DailyAchievementsFragment.java
+++ app/src/main/java/org/intelehealth/app/activities/achievements/fragments/DailyAchievementsFragment.java
@@ -19,6 +19,7 @@ import org.intelehealth.app.activities.homeActivity.HomeScreenActivity_New;
 import org.intelehealth.app.app.AppConstants;
 import org.intelehealth.app.utilities.DateAndTimeUtils;
 import org.intelehealth.app.utilities.SessionManager;
+import org.intelehealth.app.utilities.StringUtils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,8 +29,11 @@ public class DailyAchievementsFragment extends Fragment {
     public HomeScreenActivity_New activity1;
     private TextView tvPatientsCreatedToday;
     private TextView tvVisitsEndedToday;
+    private TextView tvAvgSatisfactionScore;
 
     private String todaysDate;
+    private String todaysDateInYYYYMMDD;
+
     private SessionManager sessionManager;
 
     @Override
@@ -58,12 +62,15 @@ public class DailyAchievementsFragment extends Fragment {
             startActivity(intent);
         });
 
-        TextView tvTodaysDate = view.findViewById(R.id.tv_todays_date);
         todaysDate = DateAndTimeUtils.getTodaysDateInRequiredFormat("dd MMMM, yyyy");
+        todaysDateInYYYYMMDD = DateAndTimeUtils.getTodaysDateInRequiredFormat("yyyy-MM-dd");
+
+        TextView tvTodaysDate = view.findViewById(R.id.tv_todays_date);
         tvTodaysDate.setText(todaysDate);
 
         tvPatientsCreatedToday = view.findViewById(R.id.tv_patients_created_today);
         tvVisitsEndedToday = view.findViewById(R.id.tv_visits_ended_today);
+        tvAvgSatisfactionScore = view.findViewById(R.id.tv_avg_satisfaction_score);
     }
 
     private void fetchAndSetUIData() {
@@ -71,12 +78,13 @@ public class DailyAchievementsFragment extends Fragment {
         executorService.execute(() -> {
             setPatientsCreatedToday();
             setVisitsEndedToday();
+            setAveragePatientSatisfactionScore();
         });
     }
 
     // get the number patients who were Created today as per their provider uuid
     private void setPatientsCreatedToday() {
-        String patientsCreatedTodayQuery = "SELECT COUNT(DISTINCT patientuuid ) FROM tbl_patient_attribute WHERE person_attribute_type_uuid = \"84f94425-789d-4293-a0d8-9dc01dbb4f07\" AND value = ? AND patientuuid IN (SELECT  patientuuid FROM tbl_patient_attribute WHERE person_attribute_type_uuid = \"ffc8ebee-f70c-4743-bc3c-2fe4ac843245\" AND value = ?)";
+        String patientsCreatedTodayQuery = "SELECT COUNT(DISTINCT patientuuid) FROM tbl_patient_attribute WHERE person_attribute_type_uuid = \"84f94425-789d-4293-a0d8-9dc01dbb4f07\" AND value = ? AND patientuuid IN (SELECT  patientuuid FROM tbl_patient_attribute WHERE person_attribute_type_uuid = \"ffc8ebee-f70c-4743-bc3c-2fe4ac843245\" AND value = ?)";
         SQLiteDatabase db = AppConstants.inteleHealthDatabaseHelper.getReadableDatabase();
         final Cursor todayPatientsCursor = db.rawQuery(patientsCreatedTodayQuery, new String[]{sessionManager.getProviderID(), todaysDate});
         todayPatientsCursor.moveToFirst();
@@ -87,13 +95,32 @@ public class DailyAchievementsFragment extends Fragment {
 
     // get the number of visits that were ended by the current health worker today
     private void setVisitsEndedToday() {
-        String todaysDateInFormat = DateAndTimeUtils.getTodaysDateInRequiredFormat("yyyy-MM-dd");
-        String visitsEndedTodayQuery = "SELECT COUNT(DISTINCT visituuid) FROM tbl_encounter WHERE provider_uuid = ? AND encounter_type_uuid = \"629a9d0b-48eb-405e-953d-a5964c88dc30\" AND modified_date LIKE '" + todaysDateInFormat + "%'";
+        String visitsEndedTodayQuery = "SELECT COUNT(DISTINCT visituuid) FROM tbl_encounter WHERE provider_uuid = ? AND encounter_type_uuid = \"629a9d0b-48eb-405e-953d-a5964c88dc30\" AND modified_date LIKE '" + todaysDateInYYYYMMDD + "%'";
         SQLiteDatabase db = AppConstants.inteleHealthDatabaseHelper.getReadableDatabase();
         final Cursor todayVisitsEndedCursor = db.rawQuery(visitsEndedTodayQuery, new String[]{sessionManager.getProviderID()});
+
         todayVisitsEndedCursor.moveToFirst();
         String todayVisitsEndedCount = todayVisitsEndedCursor.getString(todayVisitsEndedCursor.getColumnIndex(todayVisitsEndedCursor.getColumnName(0)));
         requireActivity().runOnUiThread(() -> tvVisitsEndedToday.setText(todayVisitsEndedCount));
         todayVisitsEndedCursor.close();
+    }
+
+    // get the average patient satisfaction score for the health worker
+    private void setAveragePatientSatisfactionScore() {
+        double totalScore = 0.0;
+        String todaysAverageSatisfactionScoreQuery = "SELECT value FROM tbl_obs WHERE conceptuuid = \"78284507-fb71-4354-9b34-046ab205e18f\" AND encounteruuid IN (SELECT uuid FROM tbl_encounter WHERE provider_uuid = ? AND modified_date LIKE '" + todaysDateInYYYYMMDD + "%')";
+        SQLiteDatabase db = AppConstants.inteleHealthDatabaseHelper.getReadableDatabase();
+
+        final Cursor satisfactionScoreCursor = db.rawQuery(todaysAverageSatisfactionScoreQuery, new String[]{sessionManager.getProviderID()});
+        if (satisfactionScoreCursor.moveToFirst()) {
+            do {
+                double currentScore = Double.parseDouble(satisfactionScoreCursor.getString(satisfactionScoreCursor.getColumnIndexOrThrow("value")));
+                totalScore = totalScore + currentScore;
+            } while (satisfactionScoreCursor.moveToNext());
+        }
+
+        final Double averageScore = totalScore / satisfactionScoreCursor.getCount();
+        requireActivity().runOnUiThread(() -> tvAvgSatisfactionScore.setText(StringUtils.formatDoubleValues(averageScore)));
+        satisfactionScoreCursor.close();
     }
 }
