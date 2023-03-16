18abb535ec8a7c9f066fd8703781015fbf77d5a3
Prajwal <prajwalwaingankar@gmail.com>
2023-03-16 18:17:38 +0530

    SCD-126: Display abdominal girth value on Vitals screen.

--- app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity.java
@@ -201,6 +201,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
     ObsDTO phyExam = new ObsDTO();
     ObsDTO height = new ObsDTO();
     ObsDTO weight = new ObsDTO();
+    ObsDTO abdominalgirth = new ObsDTO();
     ObsDTO pulse = new ObsDTO();
     ObsDTO bpSys = new ObsDTO();
     ObsDTO bpDias = new ObsDTO();
@@ -236,6 +237,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
     TextView visitView;
     TextView heightView;
     TextView weightView;
+    TextView abdominalGirthView;
     TextView pulseView;
     TextView bpView;
     TextView tempView;
@@ -1208,6 +1210,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
 
         heightView = findViewById(R.id.textView_height_value);
         weightView = findViewById(R.id.textView_weight_value);
+        abdominalGirthView = findViewById(R.id.textView_abdgirth_value);
         pulseView = findViewById(R.id.textView_pulse_value);
         bpView = findViewById(R.id.textView_bp_value);
         tempView = findViewById(R.id.textView_temp_value);
@@ -1273,6 +1276,7 @@ public class VisitSummaryActivity extends AppCompatActivity {
         }
 
         weightView.setText(weight.getValue());
+        abdominalGirthView.setText(abdominalgirth.getValue());
         pulseView.setText(pulse.getValue());
 
         String bpText = bpSys.getValue() + "/" + bpDias.getValue();
@@ -3624,6 +3628,11 @@ public class VisitSummaryActivity extends AppCompatActivity {
             case UuidDictionary.WEIGHT: //Weight
             {
                 weight.setValue(value);
+                break;
+            }
+            case UuidDictionary.ABDOMINAL_GIRTH:
+            {
+                abdominalgirth.setValue(value);
                 break;
             }
             case UuidDictionary.PULSE: //Pulse
