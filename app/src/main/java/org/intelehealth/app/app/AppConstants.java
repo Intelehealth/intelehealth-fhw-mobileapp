b9742c5c39a10257edb01c7155dfe1d84326b329
Lincon Pradhan <i.can.do.it.now.for.you@gmail.com>
2023-04-28 13:49:52 +0530

    point to dev.intelehealth.org

--- app/src/main/java/org/intelehealth/app/app/AppConstants.java
+++ app/src/main/java/org/intelehealth/app/app/AppConstants.java
@@ -128,9 +128,9 @@ public class AppConstants {
     //UI2.0 Constants
     //public static final String DEMO_URL = "uiux.intelehealth.org";
     //public static final String DEMO_URL = "demo2.intelehealth.org";
-    public static final String DEMO_URL = "demo.intelehealth.org";
+    //public static final String DEMO_URL = "demo.intelehealth.org";
 
-    //public static final String DEMO_URL = "dev.intelehealth.org";
+    public static final String DEMO_URL = "dev.intelehealth.org";
     //public static final String DEMO_URL = "testing.intelehealth.org";
 
     public static final int INTENT_FROM_AYU_FOR_SETUP = 1;
