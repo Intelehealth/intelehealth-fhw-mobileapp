1f961bd0115c0303a5865574d4eed2395ae9dc15
Mithun Vaghela <mithun@intelehealth.org>
2023-06-14 12:16:03 +0530

    InputValidationListener added

---
+++ app/src/main/java/org/intelehealth/ezazi/ui/InputValidationListener.java
@@ -0,0 +1,26 @@
+package org.intelehealth.ezazi.ui;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by Vaghela Mithun R. on 14-06-2023 - 11:54.
+ * Email : mithun@intelehealth.org
+ * Mob   : +919727206702
+ **/
+public class InputValidationListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
