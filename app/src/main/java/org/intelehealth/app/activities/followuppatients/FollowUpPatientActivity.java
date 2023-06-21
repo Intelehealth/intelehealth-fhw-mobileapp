5c58da0d24d5b8efd2088598ba9be704a1d42926
Prajwal <prajwalwaingankar@gmail.com>
2023-06-21 20:19:53 +0530

    NAS-236: Follow-up in descending order for mobile app.

--- app/src/main/java/org/intelehealth/app/activities/followuppatients/FollowUpPatientActivity.java
+++ app/src/main/java/org/intelehealth/app/activities/followuppatients/FollowUpPatientActivity.java
@@ -113,7 +113,13 @@ public class FollowUpPatientActivity extends AppCompatActivity {
         Date cDate = new Date();
         String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).format(cDate);
         String oldQuery = "SELECT * FROM " + table +" as p where p.uuid in (select v.patientuuid from tbl_visit as v where v.uuid in (select e.visituuid from tbl_encounter as e where e.uuid in (select o.encounteruuid from tbl_obs as o where o.conceptuuid = ? and o.value like '%"+ currentDate +"%')))";
-        String query = "SELECT a.uuid, a.sync, a.patientuuid, a.startdate, a.enddate, b.uuid, b.first_name, b.middle_name, b.last_name, b.date_of_birth, b.openmrs_id, c.value AS speciality, o.value FROM tbl_visit a, tbl_patient b, tbl_encounter d, tbl_obs o, tbl_visit_attribute c WHERE a.uuid = c.visit_uuid AND  a.enddate is NOT NULL AND a.patientuuid = b.uuid AND a.uuid = d.visituuid AND d.uuid = o.encounteruuid AND o.conceptuuid = ?  AND o.value is NOT NULL GROUP BY a.patientuuid";
+        String query = "SELECT a.uuid, a.sync, a.patientuuid, a.startdate, a.enddate, b.uuid, b.first_name, b.middle_name, " +
+                "b.last_name, b.date_of_birth, b.openmrs_id, c.value AS speciality, o.value, " +
+                "substr(o.value, 7,4) || '-' ||  substr(o.value, 4, 2) || '-' || substr(o.value, 1, 2) as proper_date FROM " +
+                "tbl_visit a, tbl_patient b, tbl_encounter d, tbl_obs o, tbl_visit_attribute c WHERE " +
+                "a.uuid = c.visit_uuid AND  a.enddate is NOT NULL AND a.patientuuid = b.uuid AND " +
+                "a.uuid = d.visituuid AND d.uuid = o.encounteruuid AND o.conceptuuid = ?  AND " +
+                "o.value is NOT NULL GROUP BY a.patientuuid Order By proper_date desc";
         final Cursor searchCursor = db.rawQuery(query,  new String[]{UuidDictionary.FOLLOW_UP_VISIT});  //"e8caffd6-5d22-41c4-8d6a-bc31a44d0c86"
         if (searchCursor.moveToFirst()) {
             do {
