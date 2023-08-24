942e4b448edefa7e1e41040651ed4a7f4b49467d
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-08-24 15:13:33 +0530

    Prescription title translated

--- app/src/main/java/org/intelehealth/unicef/activities/visit/PrescriptionActivity.java
+++ app/src/main/java/org/intelehealth/unicef/activities/visit/PrescriptionActivity.java
@@ -1801,7 +1801,7 @@ public class PrescriptionActivity extends BaseActivity implements NetworkUtils.I
             sharebtn.setOnClickListener(v -> {
                 if (!editText.getText().toString().equalsIgnoreCase("")) {
                     String phoneNumber = /*"+91" +*/ editText.getText().toString();
-                    String whatsappMessage = String.format("https://api.whatsapp.com/send?phone=%s&text=%s", phoneNumber, getResources().getString(R.string.hello_thankyou_for_using_intelehealth_app_to_download_click_here) + partial_whatsapp_presc_url + Uri.encode("#") + prescription_link + getString(R.string.and_enter_your_patient_id) + openmrsID_txt.getText().toString());
+                    String whatsappMessage = String.format("https://api.whatsapp.com/send?phone=%s&text=%s", phoneNumber, getResources().getString(R.string.hello_thankyou_for_using_intelehealth_app_to_download_click_here) + whatsapp_url + Uri.encode("#") + prescription_link + getString(R.string.and_enter_your_patient_id) + openmrsID_txt.getText().toString());
                     Log.v("whatsappMessage", whatsappMessage);
                     // Toast.makeText(context, R.string.whatsapp_presc_toast, Toast.LENGTH_LONG).show();
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(whatsappMessage)));
