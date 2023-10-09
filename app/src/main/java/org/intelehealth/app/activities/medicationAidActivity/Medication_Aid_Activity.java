404840c8fde02c91a4bc74b74f884ff608caf2a6
Prajwal <prajwalwaingankar@gmail.com>
2023-10-09 13:56:41 +0530

    SYR-390: Medication and Aid UI design start.

---
+++ app/src/main/java/org/intelehealth/app/activities/medicationAidActivity/Medication_Aid_Activity.java
@@ -0,0 +1,16 @@
+package org.intelehealth.app.activities.medicationAidActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.intelehealth.app.R;
+
+public class Medication_Aid_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication_aid);
+    }
+}
