952716eb981da2164425f7f13e490248ab63f80e
Mithun Vaghela <mithun@intelehealth.org>
2023-10-17 16:32:25 +0530

    loggin added in FCMMessageService

--- app/src/main/java/org/intelehealth/ekalarogya/services/FCMMessageService.kt
+++ app/src/main/java/org/intelehealth/ekalarogya/services/FCMMessageService.kt
@@ -1,9 +1,10 @@
 package org.intelehealth.ekalarogya.services
 
+import com.github.ajalt.timberkt.Timber
 import org.intelehealth.fcm.FBMessageService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
 import org.intelehealth.ekalarogya.webrtc.receiver.FCMNotificationReceiver
-import timber.log.Timber
 
 /**
  * Created by Vaghela Mithun R. on 18-09-2023 - 10:16.
@@ -13,10 +14,11 @@ import timber.log.Timber
 class FCMMessageService : FBMessageService(FCMNotificationReceiver::class.java) {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Timber.d("onNewToke ---> $token")
+        Timber.d { "onNewToke ---> $token" }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        Timber.d { "Remote message ${Gson().toJson(message)}" }
     }
 }
