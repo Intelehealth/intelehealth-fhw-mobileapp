0e39a9892021e86f7f0c4f0f87742398042924ca
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-11-09 13:12:38 +0530

    IDA4-3129: Complete

--- app/src/main/java/org/intelehealth/app/activities/identificationActivity/Fragment_SecondScreen.java
+++ app/src/main/java/org/intelehealth/app/activities/identificationActivity/Fragment_SecondScreen.java
@@ -140,6 +140,7 @@ public class Fragment_SecondScreen extends Fragment {
         mDistrictET = view.findViewById(R.id.district_edittext);
         mCityVillageET = view.findViewById(R.id.city_village_edittext);
         mCityVillageET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        mCityVillageET.setFilters(new InputFilter[]{new InputFilter.LengthFilter(50)}); //maxlength 50
         mAddress1EditText = view.findViewById(R.id.address1_edittext);
         mAddress1EditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(50)}); //maxlength 50
         mAddress2EditText = view.findViewById(R.id.address2_edittext);
