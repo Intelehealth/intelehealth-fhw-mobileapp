a467b206407df8d1ef9294da2a60adba95f21458
Mithun Vaghela <mithun@intelehealth.org>
2023-11-27 13:24:43 +0530

    Active all notification

--- app/src/main/java/org/intelehealth/ekalarogya/webrtc/receiver/FCMNotificationReceiver.kt
+++ app/src/main/java/org/intelehealth/ekalarogya/webrtc/receiver/FCMNotificationReceiver.kt
@@ -1,18 +1,27 @@
 package org.intelehealth.ekalarogya.webrtc.receiver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
+import org.intelehealth.ekalarogya.R
+import org.intelehealth.ekalarogya.activities.homeActivity.HomeActivity
 import org.intelehealth.ekalarogya.database.dao.PatientsDAO
-import org.intelehealth.ekalarogya.firebase.RealTimeDataChangedObserver
 import org.intelehealth.ekalarogya.utilities.NotificationUtils
+import org.intelehealth.ekalarogya.utilities.OfflineLogin
 import org.intelehealth.ekalarogya.utilities.SessionManager
 import org.intelehealth.ekalarogya.webrtc.activity.EkalVideoActivity
 import org.intelehealth.fcm.FcmBroadcastReceiver
+import org.intelehealth.fcm.FcmNotification
 import org.intelehealth.klivekit.call.utils.CallHandlerUtils
 import org.intelehealth.klivekit.call.utils.CallMode
-import org.intelehealth.klivekit.call.utils.CallStatus
 import org.intelehealth.klivekit.call.utils.CallType
 import org.intelehealth.klivekit.call.utils.IntentUtils
 import org.intelehealth.klivekit.model.RtcArgs
@@ -53,8 +62,80 @@ class FCMNotificationReceiver : FcmBroadcastReceiver() {
                         CallHandlerUtils.operateIncomingCall(it, arg, EkalVideoActivity::class.java)
                     }
                 }
+            } else {
+                parseMessage(notification, context)
             }
         }
     }
 
+    private fun parseMessage(notification: RemoteMessage.Notification?, context: Context) {
+        notification?.let {
+            when (notification.body) {
+                "INVALIDATE_OFFLINE_LOGIN" -> {
+                    //Invalidating Offline credentials
+                    OfflineLogin.getOfflineLogin().invalidateLoginCredentials()
+                }
+
+                "UPDATE_MIND_MAPS" -> {
+                    run {}
+                    //Calling method to generate notification
+                    sendNotification(notification, context)
+                }
+
+                else -> sendNotification(notification, context)
+            }
+        }
+    }
+
+
+    //This method is only generating push notification
+    //It is same as we did in earlier posts
+    private fun sendNotification(notification: RemoteMessage.Notification?, context: Context) {
+        val messageTitle = notification!!.title
+        val messageBody = notification.body
+        val notificationIntent = Intent(context, HomeActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            notificationIntent,
+            NotificationUtils.getPendingIntentFlag()
+        )
+
+        FcmNotification.Builder(context)
+            .channelName("EKAL")
+            .title(messageTitle ?: "Ekal")
+            .content(messageBody ?: "")
+            .smallIcon(R.mipmap.ic_launcher)
+            .contentIntent(pendingIntent)
+            .build().startNotify()
+
+//        val channelId = "CHANNEL_ID"
+//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val notificationBuilder: NotificationCompat.Builder =
+//            NotificationCompat.Builder(context, channelId)
+//                .setSmallIcon(R.mipmap.ic_launcher) //.setContentTitle("Firebase Push Notification")
+//                .setContentTitle(messageTitle)
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent)
+//                .setColor(context.getResources().getColor(R.color.white))
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//
+//        /*NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);*/
+//        val notificationManager = NotificationManagerCompat.from(context)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val name: CharSequence = "Default Channel"
+//            val description = "Default Channel description"
+//            val importance = NotificationManager.IMPORTANCE_DEFAULT
+//            val channel = NotificationChannel(channelId, name, importance)
+//            channel.description = description
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//        notificationManager.notify(1, notificationBuilder.build())
+    }
 }
