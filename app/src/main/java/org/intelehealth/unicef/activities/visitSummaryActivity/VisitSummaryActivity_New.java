23d1a827a7bdd8483a55a95f49c9e699e561de10
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-12-06 10:50:31 +0530

    Minor issue on Visit Summary activity - fixed

--- app/src/main/java/org/intelehealth/unicef/activities/visitSummaryActivity/VisitSummaryActivity_New.java
+++ app/src/main/java/org/intelehealth/unicef/activities/visitSummaryActivity/VisitSummaryActivity_New.java
@@ -607,8 +607,10 @@ public class VisitSummaryActivity_New extends BaseActivity implements AdapterInt
             doc_speciality_card.setVisibility(View.VISIBLE);
             special_vd_card.setVisibility(View.GONE);
 
-
+            hospitalTypeCard.setVisibility(View.VISIBLE);
+            specialHospitalCard.setVisibility(View.GONE);
         }
+
         btn_bottom_printshare.setVisibility(View.GONE);
         btn_bottom_vs.setVisibility(View.VISIBLE);
 
