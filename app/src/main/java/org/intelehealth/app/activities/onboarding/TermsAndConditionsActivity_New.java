cb3a6c1b1e88d64c3bcb8fb49b156547ccbc767e
Prajwal W <prajwalwaingankar@yahoo.in>
2023-12-20 14:02:16 +0530

    IDA-3237: Terms and Conditions screen lag issue fixed.

--- app/src/main/java/org/intelehealth/app/activities/onboarding/TermsAndConditionsActivity_New.java
+++ app/src/main/java/org/intelehealth/app/activities/onboarding/TermsAndConditionsActivity_New.java
@@ -7,6 +7,8 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.LocaleList;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -29,36 +31,61 @@ import java.util.Objects;
 public class TermsAndConditionsActivity_New extends AppCompatActivity {
     private static final String TAG = "TermsAndConditionsActiv";
     private int mIntentFrom;
+    JSONObject obj = null;
+    TextView tvText;
+    ImageView ivBack;
+    String privacy_string;
+    private Context context = TermsAndConditionsActivity_New.this;
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions_ui2);
-        mIntentFrom = getIntent().getIntExtra("IntentFrom", 0);
+      //  mIntentFrom = getIntent().getIntExtra("IntentFrom", 0);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.WHITE);
         }
 
+        ivBack = findViewById(R.id.iv_back_arrow_terms);
+        tvText = findViewById(R.id.tv_term_condition);
+        sessionManager = new SessionManager(context);
 
-        ImageView ivBack = findViewById(R.id.iv_back_arrow_terms);
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                finish();
             }
         });
-        TextView tvText = findViewById(R.id.tv_term_condition);
-        JSONObject obj = null;
-        try {
-            obj = new JSONObject(Objects.requireNonNullElse(
-                    FileUtils.readFileRoot(AppConstants.CONFIG_FILE_NAME, this),
-                    String.valueOf(FileUtils.encodeJSON(this, AppConstants.CONFIG_FILE_NAME)))); //Load the config file
-            String privacy_string = new SessionManager(this).getAppLanguage().equalsIgnoreCase("hi") ? obj.getString("terms_and_conditions_Hindi"):obj.getString("terms_and_conditions");
-            tvText.setText(HtmlCompat.fromHtml(privacy_string, HtmlCompat.FROM_HTML_MODE_LEGACY));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
+      //  try {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // bg task
+                    try {
+                        obj = new JSONObject(Objects.requireNonNullElse(
+                                FileUtils.readFileRoot(AppConstants.CONFIG_FILE_NAME, context),
+                                String.valueOf(FileUtils.encodeJSON(context, AppConstants.CONFIG_FILE_NAME)))); //Load the config file
+                        privacy_string = sessionManager.getAppLanguage().equalsIgnoreCase("hi") ?
+                                obj.getString("terms_and_conditions_Hindi"):obj.getString("terms_and_conditions");
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // ui task
+                            tvText.setText(HtmlCompat.fromHtml(privacy_string, HtmlCompat.FROM_HTML_MODE_LEGACY));
+                        }
+                    });
+                }
+            });
+
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
