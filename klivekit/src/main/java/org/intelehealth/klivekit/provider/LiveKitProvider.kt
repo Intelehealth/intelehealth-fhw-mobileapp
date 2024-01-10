9509473184c41d802e21b5191f0769100b8e5c51
Vaghela Mithun <mithun@intelehealth.org>
2024-01-10 18:13:16 +0530

    move to livekit old version

--- klivekit/src/main/java/org/intelehealth/klivekit/provider/LiveKitProvider.kt
+++ klivekit/src/main/java/org/intelehealth/klivekit/provider/LiveKitProvider.kt
@@ -2,7 +2,6 @@ package org.intelehealth.klivekit.provider
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
-import io.livekit.android.AudioOptions
 import io.livekit.android.LiveKit
 import io.livekit.android.LiveKitOverrides
 import io.livekit.android.RoomOptions
@@ -13,9 +12,7 @@ import io.livekit.android.room.participant.VideoTrackPublishDefaults
 import io.livekit.android.room.track.CameraPosition
 import io.livekit.android.room.track.LocalAudioTrackOptions
 import io.livekit.android.room.track.LocalVideoTrackOptions
-import io.livekit.android.room.track.VideoPreset169
 import io.livekit.android.room.track.VideoPreset43
-import org.intelehealth.klivekit.utils.AudioType
 import org.webrtc.EglBase
 import org.webrtc.HardwareVideoEncoderFactory
 
@@ -48,7 +45,7 @@ object LiveKitProvider {
     private fun provideLocalVideoTrackOptions() = LocalVideoTrackOptions(
         deviceId = "",
         position = CameraPosition.FRONT,
-        captureParams = VideoPreset43.H1440.capture,
+        captureParams = VideoPreset43.FHD.capture,
     )
 
     private fun provideAudioPublishDefault() = AudioTrackPublishDefaults(
@@ -57,7 +54,7 @@ object LiveKitProvider {
     )
 
     private fun provideVideoPublishTrack() = VideoTrackPublishDefaults(
-        videoEncoding = VideoPreset43.H540.encoding
+        videoEncoding = VideoPreset43.VGA.encoding
 //        videoEncoding = VideoPreset169.VGA.encoding,
 //            videoCodec = VideoCodec.VP8.codecName
     )
@@ -70,8 +67,8 @@ object LiveKitProvider {
     ) = RoomOptions(
         audioTrackCaptureDefaults = localAudioTrackOptions,
         audioTrackPublishDefaults = audioTrackPublishDefaults,
-        videoTrackCaptureDefaults = localVideoTrackOptions,
-        videoTrackPublishDefaults = videoTrackPublishDefaults,
+//        videoTrackCaptureDefaults = localVideoTrackOptions,
+//        videoTrackPublishDefaults = videoTrackPublishDefaults,
         adaptiveStream = true
     )
 
@@ -86,10 +83,7 @@ object LiveKitProvider {
         options = options,
         overrides = LiveKitOverrides(
             okHttpClient = RetrofitProvider.getOkHttpClient(),
-            audioOptions = AudioOptions(
-                audioHandler = audioSwitchHandler,
-                audioOutputType = io.livekit.android.AudioType.MediaAudioType()
-            ),
+            audioHandler = audioSwitchHandler,
             videoEncoderFactory = HardwareVideoEncoderFactory(
                 EglBase.create().eglBaseContext,
                 true,
