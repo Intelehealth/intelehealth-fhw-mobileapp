3af1734241549d56d2263c56a4fa7400233abb35
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-02-04 22:45:14 +0530

    Toolbar improvements on listing screen - back button support enabled

--- videolibrary/src/main/java/org/intelehealth/videolibrary/view/activities/YoutubeListingActivity.kt
+++ videolibrary/src/main/java/org/intelehealth/videolibrary/view/activities/YoutubeListingActivity.kt
@@ -1,13 +1,16 @@
 package org.intelehealth.videolibrary.view.activities
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.intelehealth.videolibrary.R
+import org.intelehealth.videolibrary.databinding.ActivityYoutubeListingBinding
 import org.intelehealth.videolibrary.model.callbacks.VideoClickedListener
 import org.intelehealth.videolibrary.model.constants.Constants
 import org.intelehealth.videolibrary.view.adapter.YoutubeListingAdapter
-import org.intelehealth.videolibrary.databinding.ActivityYoutubeListingBinding
 
 private val videoIDs: List<String> = listOf(
     "-98fnc4VAo8",
@@ -24,8 +27,22 @@ class YoutubeListingActivity : AppCompatActivity(), VideoClickedListener {
         super.onCreate(savedInstanceState)
         binding = ActivityYoutubeListingBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        setSupportActionBar(binding?.toolbar)
+        setToolbar()
         setVideoLibraryRecyclerView()
+    }
+
+    private fun setToolbar() {
+        binding?.toolbar?.apply {
+            setSupportActionBar(this)
+            setTitleTextAppearance(this@YoutubeListingActivity, R.style.ToolbarTheme)
+            setTitleTextColor(Color.WHITE)
+        }
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(true)
+            title = getString(R.string.video_library)
+        }
     }
 
     private fun setVideoLibraryRecyclerView() {
@@ -40,6 +57,14 @@ class YoutubeListingActivity : AppCompatActivity(), VideoClickedListener {
             it.setHasFixedSize(true)
             it.layoutManager = LinearLayoutManager(this@YoutubeListingActivity)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId: Int = item.itemId
+        if (itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onVideoClicked(videoId: String) {
