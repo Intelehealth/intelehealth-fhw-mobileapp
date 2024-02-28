705963add47e831d37a7e80513dcd0b146df0841
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-02-29 00:50:14 +0530

    PDF sharing facility implemented in Visit

--- app/src/main/java/org/intelehealth/ekalarogya/activities/visitSummaryActivity/VisitSummaryActivity.java
+++ app/src/main/java/org/intelehealth/ekalarogya/activities/visitSummaryActivity/VisitSummaryActivity.java
@@ -98,6 +98,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -592,56 +593,68 @@ public class VisitSummaryActivity extends BaseActivity {
 
                     String fileNamePatientName = patientName.replace(" ", "-");
                     String fileNameOpenMrsId = patient.getOpenmrs_id();
-                    String fileNameVisitID = visitUUID.substring(visitUUID.length() - 5);
+                    String fileNameVisitID = visitUuid.substring(visitUuid.length() - 5);
 
                     String fileName = fileNamePatientName.concat("-").concat(fileNameOpenMrsId).concat("-").concat(visitStartDate).concat("-").concat(fileNameVisitID).concat(".pdf");
                     buildAndSavePrescription(fileName);
 
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(VisitSummaryActivity.this);
-                    EditText editText = new EditText(VisitSummaryActivity.this);
-                    editText.setInputType(InputType.TYPE_CLASS_PHONE);
-                    InputFilter inputFilter = new InputFilter() {
-                        @Override
-                        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-                            return null;
-                        }
-                    };
-                    String partial_whatsapp_presc_url = new UrlModifiers().setwhatsappPresciptionUrl();
-                    String whatsapp_url = partial_whatsapp_presc_url.concat(visitUuid);
-//                    Spanned hyperlink_whatsapp = HtmlCompat.fromHtml("<a href=" + whatsapp_url + ">Click Here</a>", HtmlCompat.FROM_HTML_MODE_COMPACT);
+                    String message = "Here is the PDF file you requested.";
 
-                    editText.setFilters(new InputFilter[]{inputFilter, new InputFilter.LengthFilter(10)});
-                    editText.setText(patient.getPhone_number());
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    editText.setLayoutParams(layoutParams);
-                    alertDialog.setView(editText);
+                    File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+                    Uri uri = FileProvider.getUriForFile(VisitSummaryActivity.this, getApplicationContext().getPackageName() + ".provider", pdfFile);
 
-                    //AlertDialog alertDialog = new AlertDialog.Builder(context,R.style.AlertDialogStyle).create();
-                    alertDialog.setMessage(getResources().getString(R.string.enter_mobile_number_to_share_prescription));
-                    alertDialog.setPositiveButton(getResources().getString(R.string.share), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.putExtra(Intent.EXTRA_STREAM, uri);
+                    intent.setType("application/pdf");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    intent.setPackage("com.whatsapp");
+                    startActivity(intent);
 
-                            if (!editText.getText().toString().equalsIgnoreCase("")) {
-                                String phoneNumber = "+91" + editText.getText().toString();
-                                String whatsappMessage = getResources().getString(R.string.hello_thankyou_for_using_intelehealth_app_to_download_click_here) + whatsapp_url + getResources().getString(R.string.and_enter_your_patient_id) + idView.getText().toString();
-
-                                // Toast.makeText(context, R.string.whatsapp_presc_toast, Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https://api.whatsapp.com/send?phone=%s&text=%s", phoneNumber, whatsappMessage))));
-
-                                // isreturningWhatsapp = true;
-
-                            } else {
-                                Toast.makeText(context, getResources().getString(R.string.please_enter_mobile_number), Toast.LENGTH_SHORT).show();
-
-                            }
-
-                        }
-                    });
-                    AlertDialog dialog = alertDialog.show();
-                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
-                    positiveButton.setTextColor(context.getResources().getColor(R.color.colorPrimaryDark));
-                    //alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTypeface(Typeface.DEFAULT, Typeface.BOLD);
-                    IntelehealthApplication.setAlertDialogCustomTheme(context, dialog);
+//                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(VisitSummaryActivity.this);
+//                    EditText editText = new EditText(VisitSummaryActivity.this);
+//                    editText.setInputType(InputType.TYPE_CLASS_PHONE);
+//                    InputFilter inputFilter = new InputFilter() {
+//                        @Override
+//                        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+//                            return null;
+//                        }
+//                    };
+//                    String partial_whatsapp_presc_url = new UrlModifiers().setwhatsappPresciptionUrl();
+//                    String whatsapp_url = partial_whatsapp_presc_url.concat(visitUuid);
+////                    Spanned hyperlink_whatsapp = HtmlCompat.fromHtml("<a href=" + whatsapp_url + ">Click Here</a>", HtmlCompat.FROM_HTML_MODE_COMPACT);
+//
+//                    editText.setFilters(new InputFilter[]{inputFilter, new InputFilter.LengthFilter(10)});
+//                    editText.setText(patient.getPhone_number());
+//                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//                    editText.setLayoutParams(layoutParams);
+//                    alertDialog.setView(editText);
+//
+//                    //AlertDialog alertDialog = new AlertDialog.Builder(context,R.style.AlertDialogStyle).create();
+//                    alertDialog.setMessage(getResources().getString(R.string.enter_mobile_number_to_share_prescription));
+//                    alertDialog.setPositiveButton(getResources().getString(R.string.share), new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int which) {
+//
+//                            if (!editText.getText().toString().equalsIgnoreCase("")) {
+//                                String phoneNumber = "+91" + editText.getText().toString();
+////                                String whatsappMessage = getResources().getString(R.string.hello_thankyou_for_using_intelehealth_app_to_download_click_here) + whatsapp_url + getResources().getString(R.string.and_enter_your_patient_id) + idView.getText().toString();
+////
+////                                // Toast.makeText(context, R.string.whatsapp_presc_toast, Toast.LENGTH_LONG).show();
+////                                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https://api.whatsapp.com/send?phone=%s&text=%s", phoneNumber, whatsappMessage))));
+////
+////                                // isreturningWhatsapp = true;
+//
+//                            } else {
+//                                Toast.makeText(context, getResources().getString(R.string.please_enter_mobile_number), Toast.LENGTH_SHORT).show();
+//
+//                            }
+//
+//                        }
+//                    });
+//                    AlertDialog dialog = alertDialog.show();
+//                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+//                    positiveButton.setTextColor(context.getResources().getColor(R.color.colorPrimaryDark));
+//                    //alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+//                    IntelehealthApplication.setAlertDialogCustomTheme(context, dialog);
                 } else
                     showOkDismissDialog(null, getString(R.string.download_prescription_first_before_sharing), getString(R.string.ok));
 
