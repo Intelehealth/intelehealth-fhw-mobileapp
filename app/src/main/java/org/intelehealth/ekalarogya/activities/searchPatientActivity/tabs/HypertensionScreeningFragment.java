269c2767209a3396d3a4a23b72c1a2a60d4ca6ff
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-03-19 10:48:46 +0530

    Fragments created for separate tabs

---
+++ app/src/main/java/org/intelehealth/ekalarogya/activities/searchPatientActivity/tabs/HypertensionScreeningFragment.java
@@ -0,0 +1,24 @@
+package org.intelehealth.ekalarogya.activities.searchPatientActivity.tabs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.intelehealth.ekalarogya.databinding.LayoutSearchPatientCategoryBinding;
+
+public class HypertensionScreeningFragment extends Fragment {
+
+    private LayoutSearchPatientCategoryBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = LayoutSearchPatientCategoryBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+}
