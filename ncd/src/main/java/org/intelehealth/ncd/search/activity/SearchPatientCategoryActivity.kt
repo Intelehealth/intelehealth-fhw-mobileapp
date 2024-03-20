ef155059f484c4a18a389c58c4a1fc82a931218e
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-03-20 18:34:21 +0530

    Swipeable UI Implemented for Search Patient

--- ncd/src/main/java/org/intelehealth/ncd/search/activity/SearchPatientCategoryActivity.kt
+++ ncd/src/main/java/org/intelehealth/ncd/search/activity/SearchPatientCategoryActivity.kt
@@ -3,8 +3,18 @@ package org.intelehealth.ncd.search.activity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import org.intelehealth.ncd.R
 import org.intelehealth.ncd.databinding.ActivitySearchPatientCategoryBinding
+import org.intelehealth.ncd.search.pager.CategoryPagerAdapter
+import org.intelehealth.ncd.search.tabs.AnemiaFollowUpFragment
+import org.intelehealth.ncd.search.tabs.AnemiaScreeningFragment
+import org.intelehealth.ncd.search.tabs.DiabetesFollowUpFragment
+import org.intelehealth.ncd.search.tabs.DiabetesScreeningFragment
+import org.intelehealth.ncd.search.tabs.GeneralFragment
+import org.intelehealth.ncd.search.tabs.HypertensionFollowUpFragment
+import org.intelehealth.ncd.search.tabs.HypertensionScreeningFragment
 
 class SearchPatientCategoryActivity : AppCompatActivity() {
 
@@ -16,6 +26,7 @@ class SearchPatientCategoryActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         setToolbar()
+        setViewPager()
     }
 
     private fun setToolbar() {
@@ -24,5 +35,34 @@ class SearchPatientCategoryActivity : AppCompatActivity() {
             setTitleTextAppearance(this@SearchPatientCategoryActivity, R.style.ToolbarTheme)
             setTitleTextColor(Color.WHITE)
         }
+    }
+
+    private fun setViewPager() {
+        val fragmentList: List<Fragment> = listOf(
+            AnemiaScreeningFragment(),
+            AnemiaFollowUpFragment(),
+            DiabetesScreeningFragment(),
+            DiabetesFollowUpFragment(),
+            HypertensionScreeningFragment(),
+            HypertensionFollowUpFragment(),
+            GeneralFragment()
+        )
+
+        val adapter = CategoryPagerAdapter(this, fragmentList)
+        binding?.vpCategory?.adapter = adapter
+
+        val tabTitles = listOf(
+            getString(R.string.tab_anemia_screening),
+            getString(R.string.tab_anemia_follow_up),
+            getString(R.string.tab_diabetes_screening),
+            getString(R.string.tab_diabetes_follow_up),
+            getString(R.string.tab_hypertension_screening),
+            getString(R.string.tab_hypertension_follow_up),
+            getString(R.string.tab_general)
+        )
+
+        TabLayoutMediator(binding?.tlCategory!!, binding?.vpCategory!!) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
     }
 }
---
+++ ncd/src/main/java/org/intelehealth/ncd/search/tabs/AnemiaFollowUpFragment.kt
@@ -0,0 +1,22 @@
+package org.intelehealth.ncd.search.tabs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.intelehealth.ncd.databinding.LayoutSearchPatientCategoryBinding
+
+class AnemiaFollowUpFragment : Fragment() {
+
+    private var binding: LayoutSearchPatientCategoryBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = LayoutSearchPatientCategoryBinding.inflate(layoutInflater)
+        return binding!!.root
+    }
+}
