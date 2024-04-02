abab22e8355c10da892ca749c59087dc1f54adb4
hatanvirintele <mhasan@intelehealth.org>
2024-04-02 14:02:17 +0600

    Added dummy implementation on sync button crash, And other firebase issue resolved

--- app/src/main/java/org/intelehealth/app/activities/homeActivity/HomeScreenActivity_New.java
+++ app/src/main/java/org/intelehealth/app/activities/homeActivity/HomeScreenActivity_New.java
@@ -1188,8 +1188,19 @@ public class HomeScreenActivity_New extends BaseActivity implements NetworkUtils
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            unregisterReceiver(syncBroadcastReceiver);
+            //unregister receiver for internet check
+            networkUtils.unregisterNetworkReceiver();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 
-//    private void requestPermission() {
+    //    private void requestPermission() {
 //        Intent serviceIntent = new Intent(this, CallListenerBackgroundService.class);
 //        if (!CallListenerBackgroundService.isInstanceCreated()) {
 //            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
