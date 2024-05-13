1678e79baa8ae5e543805db705ee000beeb09a7c
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-05-13 11:06:04 +0530

    Activity created for searching through ncd protocols

---
+++ ncd/src/main/java/org/intelehealth/ncd/search/NcdSearchActivity.kt
@@ -0,0 +1,12 @@
+package org.intelehealth.ncd.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.intelehealth.ncd.R
+
+class NcdSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ncd_search)
+    }
+}
