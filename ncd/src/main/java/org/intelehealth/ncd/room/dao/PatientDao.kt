abe329b5b68124b0e5058536bc76dabd5b29e267
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-05-20 18:51:20 +0530

    Lower-case search added for open mrs id.

--- ncd/src/main/java/org/intelehealth/ncd/room/dao/PatientDao.kt
+++ ncd/src/main/java/org/intelehealth/ncd/room/dao/PatientDao.kt
@@ -18,7 +18,7 @@ interface PatientDao {
         "SELECT a.uuid, a.openmrs_id, a.first_name, a.middle_name, a.last_name, a.date_of_birth, b.value, b.person_attribute_type_uuid FROM tbl_patient AS a " +
                 "INNER JOIN tbl_patient_attribute AS b ON a.uuid = b.patientuuid " +
                 "WHERE b.person_attribute_type_uuid = :attribute " +
-                "AND (((a.first_name LIKE '%' || :searchString || '%') OR (a.last_name LIKE '%' || :searchString || '%') OR (a.first_name|| ' ' || a.last_name LIKE '%' || :searchString || '%') OR (a.openmrs_id = :searchString)) " +
+                "AND (((a.first_name LIKE '%' || :searchString || '%') OR (a.last_name LIKE '%' || :searchString || '%') OR (a.first_name|| ' ' || a.last_name LIKE '%' || :searchString || '%') OR (a.openmrs_id LIKE '%' || :searchString || '%')) " +
                 "OR b.patientuuid IN (SELECT c.patientuuid FROM tbl_patient_attribute as c WHERE c.person_attribute_type_uuid = :phoneNumberAttribute AND c.value = :searchString)) " +
                 "GROUP BY a.uuid"
     )
