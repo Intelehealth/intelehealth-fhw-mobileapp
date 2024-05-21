276b5d3e2c2dfea0ac17b1d81cf546837b79582b
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-05-21 22:53:37 +0530

    Minor change to fetch the latest value

--- ncd/src/main/java/org/intelehealth/ncd/room/dao/PatientDao.kt
+++ ncd/src/main/java/org/intelehealth/ncd/room/dao/PatientDao.kt
@@ -14,14 +14,52 @@ interface PatientDao {
     @Query("SELECT * FROM tbl_patient WHERE DATE('now') < DATE(date_of_birth, :age || ' years')")
     suspend fun getPatientsBelowAge(age: Int): List<Patient>
 
-    @Query(
-        "SELECT a.uuid, a.openmrs_id, a.first_name, a.middle_name, a.last_name, a.date_of_birth, b.value, b.person_attribute_type_uuid FROM tbl_patient AS a " +
-                "INNER JOIN tbl_patient_attribute AS b ON a.uuid = b.patientuuid " +
-                "WHERE b.person_attribute_type_uuid = :attribute " +
-                "AND (((a.first_name LIKE '%' || :searchString || '%') OR (a.last_name LIKE '%' || :searchString || '%') OR (a.first_name|| ' ' || a.last_name LIKE '%' || :searchString || '%') OR (a.openmrs_id LIKE '%' || :searchString || '%')) " +
-                "OR b.patientuuid IN (SELECT c.patientuuid FROM tbl_patient_attribute as c WHERE c.person_attribute_type_uuid = :phoneNumberAttribute AND c.value = :searchString)) " +
-                "GROUP BY a.uuid"
-    )
+    @Query("""
+        WITH LatestPatientAttribute AS (
+            SELECT 
+                patientuuid, 
+                MAX(modified_date) AS latest_modified_date
+            FROM 
+                tbl_patient_attribute
+            WHERE 
+                person_attribute_type_uuid = :attribute
+            GROUP BY 
+                patientuuid
+        )
+        SELECT 
+            a.uuid, 
+            a.openmrs_id, 
+            a.first_name, 
+            a.middle_name, 
+            a.last_name, 
+            a.date_of_birth, 
+            b.value, 
+            b.person_attribute_type_uuid
+        FROM 
+            tbl_patient AS a
+        INNER JOIN 
+            tbl_patient_attribute AS b
+            ON a.uuid = b.patientuuid
+        INNER JOIN 
+            LatestPatientAttribute AS lpa
+            ON b.patientuuid = lpa.patientuuid AND b.modified_date = lpa.latest_modified_date
+        WHERE 
+            b.person_attribute_type_uuid = :attribute
+            AND (
+                (a.first_name LIKE '%' || :searchString || '%'
+                OR a.last_name LIKE '%' || :searchString || '%'
+                OR a.first_name || ' ' || a.last_name LIKE '%' || :searchString || '%'
+                OR a.openmrs_id LIKE '%' || :searchString || '%')
+                OR b.patientuuid IN (
+                    SELECT c.patientuuid
+                    FROM tbl_patient_attribute AS c
+                    WHERE c.person_attribute_type_uuid = :phoneNumberAttribute
+                    AND c.value = :searchString
+                )
+            )
+        GROUP BY 
+            a.uuid
+    """)
     suspend fun queryPatientsAndAttributesForSearchString(
         attribute: String,
         searchString: String,
