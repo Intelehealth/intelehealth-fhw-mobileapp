94c4f431d8ca8a9cc4ada016907969602e1c8a08
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-09-22 20:06:09 +0530

    AEAT-1148: Complete

---
+++ app/src/main/java/org/intelehealth/app/activities/location_survey/LocationSurveyActivity.kt
@@ -0,0 +1,12 @@
+package org.intelehealth.app.activities.location_survey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.intelehealth.app.R
+
+class LocationSurveyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_survey)
+    }
+}
