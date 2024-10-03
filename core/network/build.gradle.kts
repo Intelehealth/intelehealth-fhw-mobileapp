5937095093f72fe9e58a5707f7a8406787e8e40c
Vaghela Mithun <mithun@intelehealth.org>
2024-10-03 12:41:34 +0530

    resource issues resolving

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     api("com.squareup.retrofit2:retrofit:2.9.0")
     api("com.squareup.retrofit2:converter-gson:2.9.0")
     api("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation(project(":core:utils"))
+    api(project(":core:utils"))
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
