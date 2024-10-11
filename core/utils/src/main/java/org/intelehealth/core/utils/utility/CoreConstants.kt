8dc7e44c10996bb5ea8d6b42a8104d204f569f62
Vaghela Mithun <mithun@intelehealth.org>
2024-10-11 11:51:52 +0530

    missing files committed

---
+++ core/utils/src/main/java/org/intelehealth/core/utils/utility/CoreConstants.kt
@@ -0,0 +1,8 @@
+package org.intelehealth.core.utils.utility
+
+/**
+ * Created by Vaghela Mithun R. on 12-04-2024 - 10:22.
+ * Email : mithun@intelehealth.org
+ * Mob   : +919727206702
+ **/
+const val RTC_ARGS: String = "rtc_args"
