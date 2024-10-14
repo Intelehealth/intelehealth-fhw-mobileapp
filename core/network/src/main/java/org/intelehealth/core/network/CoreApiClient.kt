2f304062a167496a67bdad9bf234e4fa0df93ae7
Prajwal W <prajwalwaingankar@yahoo.in>
2024-10-14 13:01:18 +0530

    minor change.

--- core/network/src/main/java/org/intelehealth/core/network/CoreApiClient.kt
+++ core/network/src/main/java/org/intelehealth/core/network/CoreApiClient.kt
@@ -27,27 +27,24 @@ interface CoreApiClient {
 
     @GET("location?tag=Login%20Location")
     suspend fun fetchLocationList(
-        @Query("v") representation: String)
-    : Response<Results<Location>>
+        @Query("v") representation: String
+    ): Response<Results<Location>>
 
     //EMR-Middleware/webapi/pull/pulldata/
     @GET
     suspend fun pullApiDetails(
-        @Url url: String,
-        @Header("Authorization") authHeader: String)
-    : Response<ResponseDTO>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<ResponseDTO>
 
     @GET
     suspend fun fetchLoginDetails(
-        @Url url: String,
-        @Header("Authorization") authHeader: String)
-    : Response<LoginModel>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<LoginModel>
 
     @GET
     suspend fun fetchLoginProviderDetails(
-        @Url url: String,
-        @Header("Authorization") authHeader: String)
-    : Response<LoginProviderModel>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<LoginProviderModel>
 
     @POST
     @Headers("Accept: application/json")
@@ -59,66 +56,66 @@ interface CoreApiClient {
 
     @GET
     suspend fun downloadPersonProfilePicture(
-        @Url url: String,
-        @Header("Authorization") authHeader: String)
-    : Response<ResponseBody>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
     @POST
-    suspend fun uploadPersonProfilePicture(@Url url: String,
-                                           @Header("Authorization") authHeader: String,
-                                           @Body patientProfile: PatientProfile)
-    : Response<ResponseBody>
+    suspend fun uploadPersonProfilePicture(
+        @Url url: String,
+        @Header("Authorization") authHeader: String,
+        @Body patientProfile: PatientProfile
+    ): Response<ResponseBody>
 
     // Obs Image Download
     @GET
-    suspend fun obsImageDownload(@Url url: String,
-                                 @Header("Authorization") authHeader: String) : Response<ResponseBody>
+    suspend fun obsImageDownload(
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
 
     // Fetch obs response in json format.
     @POST
     @Multipart
     @Headers("Accept: application/json")
-    suspend fun fetchObsJsonResponse(@Url url: String,
-                                     @Header("Authorization") authHeader: String,
-                                     @Part image: MultipartBody.Part,
-                                     @Part("json") obsJsonRequest: ObsPushDTO
-                                     )
-    : Response<ObsJsonResponse>
+    suspend fun fetchObsJsonResponse(
+        @Url url: String,
+        @Header("Authorization") authHeader: String,
+        @Part image: MultipartBody.Part,
+        @Part("json") obsJsonRequest: ObsPushDTO
+    ): Response<ObsJsonResponse>
 
     @DELETE
     suspend fun deleteObsImage(@Url url: String, @Header("Authorization") authHeader: String)
 
     @GET("/api/mindmap/download")
-    suspend fun downloadMindMap(@Query("key") licenseKey: String) : Response<DownloadMindMapRes>
+    suspend fun downloadMindMap(@Query("key") licenseKey: String): Response<DownloadMindMapRes>
 
     @GET("/intelehealth/app_update.json")
-    suspend fun checkAppUpdate() : Response<CheckAppUpdateRes>
+    suspend fun checkAppUpdate(): Response<CheckAppUpdateRes>
 
     @POST("/openmrs/ws/rest/v1/password")
     suspend fun changePassword(
         @Body modelChangePassword: ChangePasswordModel_New,
-        @Header("Authorization") authHeader: String)
-    : Response<ResponseBody>
+        @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
     @POST("/api/auth/requestOtp")
     @Headers("Accept: application/json")
     suspend fun requestOTP(
-        @Body modelRequestOTPParams: RequestOTPParamsModel_New)
-    : Response<ForgotPasswordApiResponseModel_New>
+        @Body modelRequestOTPParams: RequestOTPParamsModel_New
+    ): Response<ForgotPasswordApiResponseModel_New>
 
     @POST("/api/auth/verifyOtp")
     @Headers("Accept: application/json")
     suspend fun verifyOTP(
-        @Body modelOtpVerificationParams: OTPVerificationParamsModel_New)
-    : Response<ForgotPasswordApiResponseModel_New>
+        @Body modelOtpVerificationParams: OTPVerificationParamsModel_New
+    ): Response<ForgotPasswordApiResponseModel_New>
 
 
     @POST("/api/auth/resetPassword/{userUuid}")
     suspend fun resetPassword(
-        @Path("userUuid") userUuid: String,
-        @Body modelChangePassword: ChangePasswordParamsModel_New
-    ) : Response<ResetPasswordResModel_New>
+        @Path("userUuid") userUuid: String, @Body modelChangePassword: ChangePasswordParamsModel_New
+    ): Response<ResetPasswordResModel_New>
 
 
     @POST
@@ -126,14 +123,13 @@ interface CoreApiClient {
         @Url url: String,
         @Body providerProfile: ProviderProfile,
         @Header("Authorization") authHeader: String
-    ) : Response<ResponseBody>
+    ): Response<ResponseBody>
 
 
     @GET
     suspend fun downloadProviderProfilePicture(
-        @Url url: String,
-        @Header ("Authorization") authHeader: String
-    ) : Response<ResponseBody>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
     /*@GET
     Observable<Profile> PROVIDER_PROFILE_DETAILS_DOWNLOAD(@Url String url,
@@ -141,41 +137,39 @@ interface CoreApiClient {
 
     @GET
     suspend fun downloadProviderProfileDetails(
-        @Url url: String,
-        @Header ("Authorization") authHeader: String
-    ) : Response<Profile>
+        @Url url: String, @Header("Authorization") authHeader: String
+    ): Response<Profile>
 
 
     @POST("/openmrs/ws/rest/v1/person/{userUuid}")
     suspend fun updateProfileAge(
-        @Path ("userUuid") userUuid: String,
+        @Path("userUuid") userUuid: String,
         @Body profileUpdateAge: ProfileUpdateAge,
-        @Header ("Authorization") authHeader: String
-    ) : Response<ResponseBody>
+        @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
 
     @POST("/openmrs/ws/rest/v1/provider/{userUuid}/attribute")
     suspend fun createProfileAttribute(
-        @Path ("userUuid") userUuid: String,
+        @Path("userUuid") userUuid: String,
         @Body profileCreateAttribute: ProfileCreateAttribute,
-        @Header ("Authorization") authHeader: String
-    ) : Response<ResponseBody>
+        @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
 
     @POST("attribute/{attributeUuid}")
     suspend fun updateProfileAttribute(
-        @Path ("attributeUuid") attributeUuid: String,
+        @Path("attributeUuid") attributeUuid: String,
         @Body profileUpdateAttribute: ProfileUpdateAttribute,
-        @Header ("Authorization") authHeader: String
-    ) : Response<ResponseBody>
+        @Header("Authorization") authHeader: String
+    ): Response<ResponseBody>
 
 
     @POST
     @Headers("Accept: application/json")
     suspend fun authLoginJwtApi(
-        @Url url: String,
-        @Body authJWTBody: AuthJWTBody
-    ) : Response<AuthJWTResponse>
+        @Url url: String, @Body authJWTBody: AuthJWTBody
+    ): Response<AuthJWTResponse>
 
 
     /**
@@ -185,29 +179,27 @@ interface CoreApiClient {
      * @return
      */
     @GET
-    suspend fun getHtml(@Url url: String) : Response<ResponseBody>
+    suspend fun getHtml(@Url url: String): Response<ResponseBody>
 
 
     @DELETE("/api/mindmap/clearAll/{id}")
     suspend fun clearAllNotifications(
-        @Header("Authorization") authHeader: String,
-        @Path("id") id: String)
-    : Response<ResponseBody>
+        @Header("Authorization") authHeader: String, @Path("id") id: String
+    ): Response<ResponseBody>
 
 
     @PUT("/api/mindmap/acknowledge/{id}")
     suspend fun notificationsAcknowledge(
-        @Header ("Authorization") authHeader: String,
-        @Path ("id") id: String
-    ) : Response<ResponseBody>
+        @Header("Authorization") authHeader: String, @Path("id") id: String
+    ): Response<ResponseBody>
 
 
     @GET("/api/mindmap/notifications")
     suspend fun fetchAllNotifications(
-        @Header ("Authorization") authHeader: String,
+        @Header("Authorization") authHeader: String,
         @Query("userId") userId: String,
         @Query("page") page: String,
         @Query("size") size: String
-    ) : Response<NotificationResponse>
+    ): Response<NotificationResponse>
 
 }
