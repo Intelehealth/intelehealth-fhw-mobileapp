df63bc7b4baa7a406652aec7fe4eb0ef30a7d201
Prajwal W <prajwalwaingankar@yahoo.in>
2025-03-11 13:22:22 +0530

    NAS-881: For old nas 3.0 data, chief complaint data not showing issue fixed.

--- app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity_New.java
+++ app/src/main/java/org/intelehealth/app/activities/visitSummaryActivity/VisitSummaryActivity_New.java
@@ -5563,6 +5563,10 @@ public class VisitSummaryActivity_New extends BaseActivity implements AdapterInt
         // complaints data
         if (complaint.getValue() != null) {
             String value = complaint.getValue();
+            if (!value.startsWith("{") && !value.endsWith("}"))
+                value = formatHtmlToJson(value);    // NAS-881
+
+          //  value = "{\"en\":\"►<b>Fatigue and General weakness</b>: <br/>• Duration -  4 Days.<br/>• Timing - Morning.<br/>• Eating habits -  1 - patient is irregular in taking meals. Amount - Small.<br/>• Stressful condition - No.<br/>• Prior treatment sought - None.<br/>• Additional information - जेवण जात नाही.भुक लागत नाही, डोळ्यावर धुंद येत .<br/> ►<b>Headache</b>: <br/>• Duration -  4 Days.<br/>• Site - Diffuse.<br/>• Severity - Mild.<br/>• Onset - Acute onset (Patient can recall exact time when it started).<br/>• Character of headache - Stabbing, Dull continuous.<br/>• Radiation - pain does not radiate.<br/>• Timing - No particular time.<br/>• Associated illness - Hypertension.<br/>• Exacerbating factors - bending, lifting.<br/>• Prior treatment sought - None.<br/> ►<b>Associated symptoms</b>: <br/>• Patient reports -<br/> Muscle weakness,  Disturbed sleep,  Drooping eyelids,  Depressed mood,  Muscle pain,  Dizziness/Lightheadedness,  General weakness - No mood to work, Fatigue. <br/>• Patient denies -<br/> Fever,  Chills,  Night sweats,  Breathlessness on exertion,  Heat / Cold intolerance,  Jaundice,  Daytime sleepiness,  Bleeding,  Paresthesia,  Anxiety,  Joint pain,  Increase in quantity of urine output,  Increase in frequency of urination,  Polydipsia,  Polyphagia,  Vomiting with headache,  Nausea with headache,  Malaise/Discomfort,  Cough,  Cold/Sneezing,  Fainting/Loss of conciousness,  Photophobia,  Eye pain,  Visual impairment/Change in vision,  Specific weakness in particular part or side of the body<br/>\" }";
             //boolean isInOldFormat = true;
             //Show Visit summary data in Clinical Format for English language only
             //Else for other language keep the data in Question Answer format
@@ -5779,6 +5783,22 @@ public class VisitSummaryActivity_New extends BaseActivity implements AdapterInt
         }
         // family history - end
         // medical history data - end
+    }
+
+    private String formatHtmlToJson(String value) {
+        // Convert to JSON
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("en", value);
+        } catch (Exception e) {
+            Timber.tag(TAG).e(String.valueOf(e));
+        }
+
+        // Output JSON string
+        if (jsonObject.length() > 0)
+            return jsonObject.toString();
+        else
+            return value;
     }
 
     List<String> mChiefComplainList = new ArrayList<>();
