735696ce341524c6c62e086903a86510d0446da0
zKaveri <kaveri@intelehealth.org>
2025-06-27 15:06:38 +0530

    Db schema changes for ncd

--- ncd/src/main/java/org/intelehealth/ncd/data/category/CategoryDataSource.kt
+++ ncd/src/main/java/org/intelehealth/ncd/data/category/CategoryDataSource.kt
@@ -1,13 +1,19 @@
 package org.intelehealth.ncd.data.category
 
+import androidx.lifecycle.LiveData
 import org.intelehealth.ncd.model.Patient
 import org.intelehealth.ncd.model.PatientAttributes
+import org.intelehealth.ncd.model.PatientVisitDetails
+import org.intelehealth.ncd.model.Visit
 import org.intelehealth.ncd.room.dao.PatientAttributeDao
 import org.intelehealth.ncd.room.dao.PatientDao
+import org.intelehealth.ncd.room.dao.VisitDao
 
 class CategoryDataSource(
     private val patientDao: PatientDao,
-    private val patientAttributeDao: PatientAttributeDao
+    private val patientAttributeDao: PatientAttributeDao,
+    private val visitDao: VisitDao
+
 ) {
 
     suspend fun getPatientsBasedOnAge(age: Int): List<Patient> =
@@ -20,5 +26,9 @@ class CategoryDataSource(
         attributeUuid: String
     ): PatientAttributes =
         patientAttributeDao.getPatientsBasedOnAttributeUuids(patientUuid, attributeUuid)
+
+    suspend fun getStartVisitNoteEncounterByVisitUUID(visitUuid: String, encounterTypeUuid: String): String = visitDao.getStartVisitNoteEncounterByVisitUUID(visitUuid, encounterTypeUuid)
+    suspend fun getPatientVisitRawData(age: Int,  attributeTypeUuid: String, visitNoteEncounterUuid: String): List<PatientVisitDetails> = visitDao.getPatientVisitRawData(age, attributeTypeUuid,visitNoteEncounterUuid)
+    suspend fun getPatientVisitRawDataBelowAgeForGeneral(age: Int, visitNoteEncounterUuid: String): List<PatientVisitDetails> = visitDao.getPatientVisitRawDataBelowAgeForGeneral(age,visitNoteEncounterUuid)
 
 }
