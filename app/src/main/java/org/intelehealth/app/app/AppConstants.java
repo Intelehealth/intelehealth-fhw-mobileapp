bd772326e64d00944041b2863a3903a02a454ac7
hatanvirintele <mhasan@intelehealth.org>
2025-07-16 18:49:10 +0600

    Some tamil translation added

--- app/src/main/java/org/intelehealth/app/app/AppConstants.java
+++ app/src/main/java/org/intelehealth/app/app/AppConstants.java
@@ -309,5 +309,6 @@ public class AppConstants {
     public static final String LANGUAGE_KN = "kn";
     public static final String LANGUAGE_OR = "or";
     public static final String LANGUAGE_AS = "as";
+    public static final String LANGUAGE_TA = "ta";
 
 }
