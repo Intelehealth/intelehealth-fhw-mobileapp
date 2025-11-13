660a55bef7dac59ed6db3d91c7b72678c4d691b5
Lincon Pradhan <i.can.do.it.now.for.you@gmail.com>
2025-11-13 16:02:41 +0530

    cleanup the test fhir json labels

--- ncd/src/main/java/org/intelehealth/ncd/fhir/CommonQuestionnaireActivity.kt
+++ ncd/src/main/java/org/intelehealth/ncd/fhir/CommonQuestionnaireActivity.kt
@@ -66,7 +66,7 @@ class CommonQuestionnaireActivity : AppCompatActivity() {
     // for the questionnaire
     private val questionnaireFiles =
         listOf(
-            "Abdominal distention_fhir.json",
+//            "Abdominal distention_fhir.json",
             "hypertension_screening.json",
             "anemia_screening.json",
             "diabetes_screening.json",
@@ -74,7 +74,7 @@ class CommonQuestionnaireActivity : AppCompatActivity() {
         )
     private val questionnaireTitles =
         listOf(
-            "Abdominal distention",
+//            "Abdominal distention",
             "Hypertension Screening",
             "Anemia Screening",
             "Diabetes Screening",
@@ -111,7 +111,7 @@ class CommonQuestionnaireActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         val questionnaireTitlesResources = listOf(
-            "Abdominal distention",
+//            "Abdominal distention",
             getString(R.string.questionnaire_title_hypertension_screening),
             getString(R.string.questionnaire_title_anemia_screening),
             getString(R.string.questionnaire_title_diabetes_screening),
