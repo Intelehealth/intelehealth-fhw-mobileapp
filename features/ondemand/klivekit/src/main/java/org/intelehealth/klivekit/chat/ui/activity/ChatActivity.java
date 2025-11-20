42f4a4ed9a2321797746cd025e0017b4c205f1fb
Nivetha <nivetha@intelehealth.org>
2025-11-20 13:46:21 +0530

    Version Changes to 36 and UI Check - Final change

--- features/ondemand/klivekit/src/main/java/org/intelehealth/klivekit/chat/ui/activity/ChatActivity.java
+++ features/ondemand/klivekit/src/main/java/org/intelehealth/klivekit/chat/ui/activity/ChatActivity.java
@@ -27,6 +27,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -122,6 +123,7 @@ public class ChatActivity extends AppCompatActivity {
     private String openMrsId;
 
     protected LinearLayout mEmptyLinearLayout, mLoadingLinearLayout;
+    protected RelativeLayout toolbarRelative;
     protected EditText mMessageEditText;
     protected TextView mEmptyTextView;
 
@@ -135,6 +137,7 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     protected void initiateView() {
+
         mEmptyTextView = findViewById(R.id.empty_tv);
         mMessageEditText = findViewById(R.id.text_etv);
         mLoadingLinearLayout = findViewById(R.id.loading_layout);
@@ -284,10 +287,17 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getContentResourceId());
+        toolbarRelative = findViewById(R.id.header_ll);
         WindowInsetsControllerCompat controller =
                 new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
         controller.setAppearanceLightStatusBars(true);
         controller.setAppearanceLightNavigationBars(true);
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_parent), (view, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left,0, systemBars.right, systemBars.bottom);
+            toolbarRelative.setPadding(systemBars.left,systemBars.top, systemBars.right, 0);
+            return WindowInsetsCompat.CONSUMED;
+        });*/
 
         preferenceHelper = new PreferenceHelper(ChatActivity.this);
         mImagePathRoot = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator;
