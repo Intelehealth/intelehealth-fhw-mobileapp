1543b0b93cb575afaca28ba7d05a6987d2732edf
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2026-02-13 11:04:52 +0530

    Initial commit for abdm library.

---
+++ features/ondemand/abdm/src/main/java/com/example/abdm/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.abdm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
